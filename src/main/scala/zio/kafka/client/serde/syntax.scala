5630ad857224bf4de0cf9f6588d2e93a7cf93f79
vroste <svroonland@gmail.com>
2019-09-28 17:59:42 +0200

    Scala 2.11 support: expand SAMs

--- src/main/scala/zio/kafka/client/serde/syntax.scala
+++ src/main/scala/zio/kafka/client/serde/syntax.scala
@@ -3,8 +3,13 @@ import zio.Task
 
 object syntax {
   implicit class DeseralizerOps[T](val deserializer: Deserializer[T]) extends AnyVal {
-    def map[U](f: T => U): Deserializer[U]        = deserializer.deserialize(_).map(f)
-    def mapM[U](f: T => Task[U]): Deserializer[U] = deserializer.deserialize(_).flatMap(f)
+    def map[U](f: T => U): Deserializer[U] = new Deserializer[U] {
+      override def deserialize(data: Array[Byte]): Task[U] = deserializer.deserialize(data).map(f)
+    }
+
+    def mapM[U](f: T => Task[U]): Deserializer[U] = new Deserializer[U] {
+      override def deserialize(data: Array[Byte]): Task[U] = deserializer.deserialize(data).flatMap(f)
+    }
 
     /**
      * Serde that handles deserialization failures by returning a Left of a throwable
@@ -13,12 +18,18 @@ object syntax {
      * much sense
      * @return
      */
-    def either: Deserializer[Either[Throwable, T]] = deserializer.deserialize(_).either
+    def either: Deserializer[Either[Throwable, T]] = new Deserializer[Either[Throwable, T]] {
+      override def deserialize(data: Array[Byte]): Task[Either[Throwable, T]] = deserializer.deserialize(data).either
+    }
   }
 
   implicit class SerializerOps[T](val serializer: Serializer[T]) extends AnyVal {
-    def contramap[U](f: U => T): Serializer[U]        = value => serializer.serialize(f(value))
-    def contramapM[U](f: U => Task[T]): Serializer[U] = f(_) >>= serializer.serialize
+    def contramap[U](f: U => T): Serializer[U] = new Serializer[U] {
+      override def serialize(value: U): Task[Array[Byte]] = serializer.serialize(f(value))
+    }
+    def contramapM[U](f: U => Task[T]): Serializer[U] = new Serializer[U] {
+      override def serialize(value: U): Task[Array[Byte]] = f(value) >>= serializer.serialize
+    }
   }
 
   implicit class SerdeOps[T](val serde: Serde[T]) extends AnyVal {
@@ -27,7 +38,7 @@ object syntax {
      * Converts to a Serde of type U with pure transformations
      */
     def inmap[U](f: T => U)(g: U => T): Serde[U] =
-      Serde(serde.map(f))(value => serde.serialize(g(value)))
+      Serde(serde.map(f))(serde.contramap(g))
 
     /**
      * Convert to a Serde of type U with an effectful transformation
