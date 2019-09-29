0dd07e248245c1153e38251daababf3f03242a47
vroste <svroonland@gmail.com>
2019-09-29 19:14:17 +0200

    Shorter Serde names

--- src/main/scala/zio/kafka/client/serde/Serdes.scala
+++ src/main/scala/zio/kafka/client/serde/Serdes.scala
@@ -15,16 +15,16 @@ trait Serdes {
     override def deserialize(data: Array[Byte]): Task[T] = Task(serde.deserializer().deserialize(dummyTopic, data))
   }
 
-  implicit val longSerde: Serde[Any, Long]   = fromKafkaSerde(KafkaSerdes.Long()).inmap(Long2long)(long2Long)
-  implicit val intSerde: Serde[Any, Int]     = fromKafkaSerde(KafkaSerdes.Integer()).inmap(Integer2int)(int2Integer)
-  implicit val shortSerde: Serde[Any, Short] = fromKafkaSerde(KafkaSerdes.Short()).inmap(Short2short)(short2Short)
-  implicit val floatSerde: Serde[Any, Float] = fromKafkaSerde(KafkaSerdes.Float()).inmap(Float2float)(float2Float)
-  implicit val doubleSerde: Serde[Any, Double] =
+  implicit val long: Serde[Any, Long]   = fromKafkaSerde(KafkaSerdes.Long()).inmap(Long2long)(long2Long)
+  implicit val int: Serde[Any, Int]     = fromKafkaSerde(KafkaSerdes.Integer()).inmap(Integer2int)(int2Integer)
+  implicit val short: Serde[Any, Short] = fromKafkaSerde(KafkaSerdes.Short()).inmap(Short2short)(short2Short)
+  implicit val float: Serde[Any, Float] = fromKafkaSerde(KafkaSerdes.Float()).inmap(Float2float)(float2Float)
+  implicit val double: Serde[Any, Double] =
     fromKafkaSerde(KafkaSerdes.Double()).inmap(Double2double)(double2Double)
-  implicit val stringSerde: Serde[Any, String]         = fromKafkaSerde(KafkaSerdes.String())
-  implicit val byteArraySerde: Serde[Any, Array[Byte]] = fromKafkaSerde(KafkaSerdes.ByteArray())
-  implicit val byteBufferSerde: Serde[Any, ByteBuffer] = fromKafkaSerde(KafkaSerdes.ByteBuffer())
-  implicit val uuidSerde: Serde[Any, UUID]             = fromKafkaSerde(KafkaSerdes.UUID())
+  implicit val string: Serde[Any, String]         = fromKafkaSerde(KafkaSerdes.String())
+  implicit val byteArray: Serde[Any, Array[Byte]] = fromKafkaSerde(KafkaSerdes.ByteArray())
+  implicit val byteBuffer: Serde[Any, ByteBuffer] = fromKafkaSerde(KafkaSerdes.ByteBuffer())
+  implicit val uuid: Serde[Any, UUID]             = fromKafkaSerde(KafkaSerdes.UUID())
 
   implicit def deserializerWithError[R, T](implicit deser: Deserializer[R, T]): Deserializer[R, Try[T]] =
     deser.asTry
