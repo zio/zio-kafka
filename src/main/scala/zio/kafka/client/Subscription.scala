453c41e76f824ff3434e64424a55f207799d6f3a
Piotr Gabara <piotr.x.gabara@gmail.com>
2019-10-01 07:18:19 +0200

    Add nicer constructors for the Subscription terms (#38)

--- src/main/scala/zio/kafka/client/Subscription.scala
+++ src/main/scala/zio/kafka/client/Subscription.scala
@@ -1,8 +1,23 @@
 package zio.kafka.client
+
+import zio.Task
 import scala.util.matching.Regex
+import java.util.regex.{ Pattern => JPattern }
 
 sealed trait Subscription
 object Subscription {
   case class Topics(topics: Set[String]) extends Subscription
   case class Pattern(pattern: Regex)     extends Subscription
+
+  def topics(name: String, names: String*): Subscription =
+    Topics(Set(name) ++ names.toSet)
+
+  def pattern(pattern: Regex): Subscription =
+    Pattern(pattern)
+
+  def pattern(pattern: String): Task[Subscription] =
+    Task.effect(new Regex(pattern)).map(Pattern)
+
+  def pattern(pattern: JPattern): Subscription =
+    Pattern(new Regex(pattern.pattern()))
 }
