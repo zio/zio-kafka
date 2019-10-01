f1cff503ea49354f6b331e0ee8543b88e8bb28a1
vroste <svroonland@gmail.com>
2019-10-01 18:30:39 +0200

    Some docs

--- src/main/scala/zio/kafka/client/serde/Serde.scala
+++ src/main/scala/zio/kafka/client/serde/Serde.scala
@@ -1,33 +1,81 @@
 package zio.kafka.client.serde
 import zio.RIO
 
+/**
+ * Deserializer from byte array to a value of some type T
+ *
+ * @tparam R Environment available to the deserializer
+ * @tparam T Value type
+ */
 trait Deserializer[-R, +T] {
   def deserialize(data: Array[Byte]): RIO[R, T]
 }
 
 object Deserializer {
-  def of[R, T](implicit deser: Deserializer[R, T]): Deserializer[R, T] = deser
+
+  /**
+   * Obtain an instance of a Deserializer of some type from the implicit scope
+   */
+  def of[T](implicit deser: Deserializer[Any, T]): Deserializer[Any, T] = deser
+
+  /**
+   * Create a deserializer from a function
+   */
   def apply[R, T](deser: Array[Byte] => RIO[R, T]): Deserializer[R, T] = new Deserializer[R, T] {
     override def deserialize(data: Array[Byte]): RIO[R, T] = deser(data)
   }
 }
 
+/**
+ * Serializer from values of some type T to a byte array
+ *
+ * @tparam R Environment available to the serializer
+ * @tparam T
+ */
 trait Serializer[-R, -T] {
   def serialize(value: T): RIO[R, Array[Byte]]
 }
 
 object Serializer {
-  def of[R, T](implicit ser: Serializer[R, T]): Serializer[R, T] = ser
+
+  /**
+   * Obtain an instance of a Serializer of some type from the implicit scope
+   */
+  def of[T](implicit ser: Serializer[Any, T]): Serializer[Any, T] = ser
+
+  /**
+   * Create a serializer from a function
+   */
   def apply[R, T](ser: T => RIO[R, Array[Byte]]): Serializer[R, T] = new Serializer[R, T] {
     override def serialize(value: T): RIO[R, Array[Byte]] = ser(value)
   }
 }
 
+/**
+ * A serializer and deserializer for values of type T
+ *
+ * @tparam R Environment available to the deserializer
+ * @tparam T Value type
+ */
 trait Serde[-R, T] extends Deserializer[R, T] with Serializer[R, T]
 
 object Serde {
   def of[T](implicit serde: Serde[Any, T]): Serde[Any, T] = serde
 
+  /**
+   * Create a Serde from a deserializer and serializer function
+   *
+   * The (de)serializer functions can returned a failure ZIO with a Throwable to indicate (de)serialization failure
+   */
+  def apply[R, T](deser: Array[Byte] => RIO[R, T])(ser: T => RIO[R, Array[Byte]]): Serde[R, T] =
+    new Serde[R, T] {
+      override def serialize(value: T): RIO[R, Array[Byte]]  = ser(value)
+      override def deserialize(data: Array[Byte]): RIO[R, T] = deser(data)
+    }
+
+  /**
+   * Create a Serde from a deserializer and serializer function
+   */
   def apply[R, T](deser: Deserializer[R, T])(ser: Serializer[R, T]): Serde[R, T] = new Serde[R, T] {
     override def serialize(value: T): RIO[R, Array[Byte]]  = ser.serialize(value)
     override def deserialize(data: Array[Byte]): RIO[R, T] = deser.deserialize(data)
--- src/main/scala/zio/kafka/client/serde/SerdeSyntax.scala
+++ src/main/scala/zio/kafka/client/serde/SerdeSyntax.scala
@@ -16,8 +16,15 @@ trait SerdeSyntax {
 }
 
 final class DeserializerOps[R, T](private val deserializer: Deserializer[R, T]) extends AnyVal {
+
+  /**
+   * Create a deserializer for a type U based on the deserializer for type T and a mapping function
+   */
   def map[U](f: T => U): Deserializer[R, U] = Deserializer(deserializer.deserialize(_).map(f))
 
+  /**
+   * Create a deserializer for a type U based on the deserializer for type T and an effectful mapping function
+   */
   def mapM[R1 <: R, U](f: T => RIO[R1, U]): Deserializer[R1, U] = Deserializer(deserializer.deserialize(_).flatMap(f))
 
   /**
@@ -30,8 +37,15 @@ final class DeserializerOps[R, T](private val deserializer: Deserializer[R, T]) 
 }
 
 final class SerializerOps[R, T](private val serializer: Serializer[R, T]) extends AnyVal {
+
+  /**
+   * Create a serializer for a type U based on the serializer for type T and a mapping function
+   */
   def contramap[U](f: U => T): Serializer[R, U] = Serializer(f andThen serializer.serialize)
 
+  /**
+   * Create a serializer for a type U based on the serializer for type T and an effectful mapping function
+   */
   def contramapM[R1 <: R, U](f: U => RIO[R1, T]): Serializer[R1, U] = Serializer(f(_).flatMap(serializer.serialize))
 }
 
@@ -44,7 +58,7 @@ final class SerdeOps[R, T](private val serde: Serde[R, T]) extends AnyVal {
     Serde(serde.map(f))(serde.contramap(g))
 
   /**
-   * Convert to a Serde of type U with an effectful transformation
+   * Convert to a Serde of type U with effectful transformations
    */
   def inmapM[R1 <: R, U](f: T => RIO[R1, U])(g: U => RIO[R1, T]): Serde[R1, U] =
     Serde(serde.mapM(f))(serde.contramapM(g))
