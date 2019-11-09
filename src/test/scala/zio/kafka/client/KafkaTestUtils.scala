bf051783aa3ea1765ce5d23fff6a44040ddfe68c
tim <tim.pigden@optrak.com>
2019-11-09 09:46:47 +0000

    got all consumer tests working in new format

--- src/test/scala/zio/kafka/client/KafkaTestUtils.scala
+++ src/test/scala/zio/kafka/client/KafkaTestUtils.scala
@@ -1,17 +1,16 @@
 package zio.kafka.client
 
-import net.manub.embeddedkafka.{ EmbeddedK, EmbeddedKafka }
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import zio.{ Chunk, Managed, RIO, UIO, ZIO, ZManaged }
+import net.manub.embeddedkafka.EmbeddedKafka
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.TopicPartition
+import zio.{Chunk, Managed, RIO, ZIO}
 import org.apache.kafka.clients.producer.ProducerRecord
 import zio.blocking.Blocking
 import zio.clock.Clock
 import zio.kafka.client.serde.Serde
 import zio.duration._
-import zio.kafka.client.KafkaAdmin.KafkaAdminClientConfig
 import zio.kafka.client.Kafka.KafkaTestEnvironment
-import zio.random.Random
-import zio.test.environment.{ Live, TestEnvironment }
+import zio.test.environment.{Live, TestEnvironment}
 
 trait Kafka {
   def kafka: Kafka.Service
@@ -20,29 +19,15 @@ trait Kafka {
 object Kafka {
   trait Service {
     def bootstrapServers: List[String]
-    def stop(): UIO[Unit]
   }
 
-  case class EmbeddedKafkaService(embeddedK: EmbeddedK) extends Kafka.Service {
-    override def bootstrapServers: List[String] = List(s"localhost:${embeddedK.config.kafkaPort}")
-    override def stop(): UIO[Unit]              = ZIO.effectTotal(embeddedK.stop(true))
-  }
-
-  case object DefaultLocal extends Kafka.Service {
-    override def bootstrapServers: List[String] = List(s"localhost:9092")
-
-    override def stop(): UIO[Unit] = UIO.unit
-  }
-
-  val makeEmbedded: Managed[Nothing, Kafka] =
-    ZManaged.make(ZIO.effectTotal(new Kafka {
-      override val kafka: Service = EmbeddedKafkaService(EmbeddedKafka.start())
-    }))(_.kafka.stop())
-
-  val makeLocal: Managed[Nothing, Kafka] =
-    ZManaged.make(ZIO.effectTotal(new Kafka {
-      override val kafka: Service = DefaultLocal
-    }))(_.kafka.stop())
+  val make: Managed[Nothing, Kafka] =
+    Managed.fromEffect( ZIO.effectTotal( new Kafka {
+      override val kafka: Service = new Kafka.Service {
+        val embeddedK = EmbeddedKafka.start()
+        override def bootstrapServers: List[String] = List(s"localhost:${embeddedK.config.kafkaPort}")
+      }
+    }))
 
   type KafkaTestEnvironment = Kafka with TestEnvironment
 
@@ -50,13 +35,13 @@ object Kafka {
 
   def liveClockBlocking: ZIO[KafkaTestEnvironment, Nothing, KafkaClockBlocking] =
     for {
-      clck    <- Live.live(ZIO.environment[Clock])
+      clck <- Live.live(ZIO.environment[Clock])
       blcking <- ZIO.environment[Blocking]
-      kfka    <- ZIO.environment[Kafka]
+      kfka <- ZIO.environment[Kafka]
     } yield new Kafka with Clock with Blocking {
-      override val kafka: Service = kfka.kafka
+      override def kafka: Service = kfka.kafka
 
-      override val clock: Clock.Service[Any]       = clck.clock
+      override val clock: Clock.Service[Any] = clck.clock
       override val blocking: Blocking.Service[Any] = blcking.blocking
     }
 
@@ -64,10 +49,10 @@ object Kafka {
 
 object KafkaTestUtils {
 
-  def kafkaEnvironment(kafkaE: Managed[Nothing, Kafka]): Managed[Nothing, KafkaTestEnvironment] =
+  val kafkaEnvironment: Managed[Nothing, KafkaTestEnvironment] =
     for {
       testEnvironment <- TestEnvironment.Value
-      kafkaS          <- kafkaE
+      kafkaService    <- Kafka.make
     } yield new TestEnvironment(
       testEnvironment.blocking,
       testEnvironment.clock,
@@ -77,14 +62,9 @@ object KafkaTestUtils {
       testEnvironment.sized,
       testEnvironment.system
     ) with Kafka {
-      val kafka = kafkaS.kafka
+      val kafka = kafkaService.kafka
     }
 
-  val embeddedKafkaEnvironment: Managed[Nothing, KafkaTestEnvironment] =
-    kafkaEnvironment(Kafka.makeEmbedded)
-
-  val localKafkaEnvironment: Managed[Nothing, KafkaTestEnvironment] =
-    kafkaEnvironment(Kafka.makeLocal)
 
   def producerSettings =
     for {
@@ -95,45 +75,41 @@ object KafkaTestUtils {
       Map.empty
     )
 
-  def withProducer[A, K, V](
-    r: Producer[Any, K, V] => RIO[Any with Clock with Kafka with Blocking, A],
-    kSerde: Serde[Any, K],
-    vSerde: Serde[Any, V]
-  ): RIO[KafkaTestEnvironment, A] =
+  def withProducer[A, K, V](r: Producer[Any, K, V] => RIO[Any with Clock with Kafka with Blocking, A],
+                            kSerde: Serde[Any, K],
+                            vSerde: Serde[Any, V]): RIO[KafkaTestEnvironment, A] =
     for {
       settings <- producerSettings
       producer = Producer.make(settings, kSerde, vSerde)
-      lcb      <- Kafka.liveClockBlocking
-      produced <- producer.use { p =>
-                   r(p).provide(lcb)
-                 }
+      lcb <- Kafka.liveClockBlocking
+      produced <- producer.use { p => r(p).provide(lcb)}
     } yield produced
 
-  def withProducerStrings[A](r: Producer[Any, String, String] => RIO[Any with Clock with Kafka with Blocking, A]) =
-    withProducer(r, Serde.string, Serde.string)
+  def withProducerStrings[A](r: Producer[Any, String, String] => RIO[Any with Clock with Kafka with Blocking, A])
+    = withProducer(r, Serde.string, Serde.string)
 
   def produceOne(t: String, k: String, m: String) =
     withProducerStrings { p =>
-      p.produce(new ProducerRecord(t, k, m))
-    }.flatten
+        p.produce(new ProducerRecord(t, k, m))
+    }
 
-  def produceMany(t: String, kvs: Iterable[(String, String)]) =
+  def produceMany(t: String, kvs: List[(String, String)]) =
     withProducerStrings { p =>
       val records = kvs.map {
         case (k, v) => new ProducerRecord[String, String](t, k, v)
       }
       val chunk = Chunk.fromIterable(records)
       p.produceChunk(chunk)
-    }.flatten
+    }
 
-  def produceMany(topic: String, partition: Int, kvs: Iterable[(String, String)]) =
+  def produceMany(topic: String, partition: Int, kvs: List[(String, String)]) =
     withProducerStrings { p =>
       val records = kvs.map {
         case (k, v) => new ProducerRecord[String, String](topic, partition, null, k, v)
       }
       val chunk = Chunk.fromIterable(records)
       p.produceChunk(chunk)
-    }.flatten
+    }
 
   def consumerSettings(groupId: String, clientId: String) =
     for {
@@ -149,63 +125,44 @@ object KafkaTestUtils {
       ),
       250.millis,
       250.millis,
-      100
+      1
     )
 
-  def consumeWithStrings(groupId: String, clientId: String, subscription: Subscription)(
-    r: (String, String) => ZIO[Any with Kafka with Clock with Blocking, Nothing, Unit]
-  ): RIO[KafkaTestEnvironment, Unit] =
+  def consumeWithStrings(groupId: String,
+                         clientId: String,
+                         subscription: Subscription)
+                        (r: (String, String) => ZIO[Any with Kafka with Clock with Blocking, Nothing, Unit]): RIO[KafkaTestEnvironment, Unit] =
     for {
       lcb <- Kafka.liveClockBlocking
       inner <- (for {
-                settings <- consumerSettings(groupId, clientId)
-                consumed <- Consumer.consumeWith(settings, subscription, Serde.string, Serde.string)(r)
-              } yield consumed)
-                .provide(lcb)
+      settings <- consumerSettings(groupId, clientId)
+      consumed <- Consumer.consumeWith(settings, subscription, Serde.string, Serde.string)(r)
+      } yield consumed)
+        .provide(lcb)
     } yield inner
 
-  def withConsumer[A](groupId: String, clientId: String)(
-    r: Consumer => RIO[Any with Kafka with Clock with Blocking, A]
-  ): RIO[KafkaTestEnvironment, A] =
+  def withConsumer[A](groupId: String, clientId: String)
+                     (r: Consumer => RIO[Any with Kafka with Clock with Blocking, A]): RIO[KafkaTestEnvironment, A] =
     for {
       lcb <- Kafka.liveClockBlocking
       inner <- (for {
-                settings <- consumerSettings(groupId, clientId)
-                consumer = Consumer.make(settings)
-                consumed <- consumer.use { p =>
-                             r(p).provide(lcb)
-                           }
-              } yield consumed).provide(lcb)
+        settings <- consumerSettings(groupId, clientId)
+        consumer = Consumer.make(settings)
+        produced <- consumer.use { p => r(p).provide(lcb) }
+      } yield produced).provide(lcb)
     } yield inner
 
-  def adminSettings =
-    for {
-      servers <- ZIO.access[Kafka](_.kafka.bootstrapServers)
-    } yield KafkaAdminClientConfig(servers)
+  def recordsFromAllTopics[K, V](
+    pollResult: Map[TopicPartition, Chunk[ConsumerRecord[K, V]]]
+  ): Chunk[ConsumerRecord[K, V]] =
+    Chunk.fromIterable(pollResult.values).flatMap(identity)
 
-  def withAdmin[T](f: KafkaAdmin.Service => RIO[Any with Clock with Kafka with Blocking, T]) =
-    for {
-      settings <- adminSettings
-      lcb      <- Kafka.liveClockBlocking
-      fRes <- KafkaAdmin
-               .make(settings)
-               .use { admin =>
-                 f(admin.kafkaAdmin)
-               }
-               .provide(lcb)
-    } yield fRes
+  def getAllRecordsFromMultiplePolls[K, V](
+    res: List[Map[TopicPartition, Chunk[ConsumerRecord[K, V]]]]
+  ): Chunk[ConsumerRecord[K, V]] =
+    res.foldLeft[Chunk[ConsumerRecord[K, V]]](Chunk.empty)(
+      (acc, pollResult) => acc ++ recordsFromAllTopics[K, V](pollResult)
+    )
 
-  // temporary workaround for zio issue #2166 - broken infinity
-  val veryLongTime = Duration.fromNanos(Long.MaxValue)
-
-  def randomThing(prefix: String) =
-    for {
-      random <- ZIO.environment[Random]
-      l      <- random.random.nextLong(8)
-    } yield s"$prefix-$l"
-
-  def randomTopic = randomThing("topic")
-
-  def randomGroup = randomThing("group")
-
+  def tp(topic: String, partition: Int): TopicPartition = new TopicPartition(topic, partition)
 }
