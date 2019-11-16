b685ce27dd3b5bcd25a9b9ae81761da7a214fdf7
tim <tim.pigden@optrak.com>
2019-11-16 10:06:20 +0000

    new branch to allow version in publishLocal

--- src/test/scala/zio/kafka/client/LocalMultiConsumerTest.scala
+++ src/test/scala/zio/kafka/client/LocalMultiConsumerTest.scala
@@ -8,6 +8,6 @@ import zio.test.{ suite, DefaultRunnableSpec }
  */
 object LocalMultiConsumerTest
     extends DefaultRunnableSpec(
-      suite("consumer test suite3 - parallel consumers")(
+      suite("consumer test suite3 - parallel consumers local running kafka (empty to pass ci)")(
         ).provideManagedShared(KafkaTestUtils.localKafkaEnvironment)
     )
