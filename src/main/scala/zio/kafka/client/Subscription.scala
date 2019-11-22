17b28a59f9f039be562ec8c3b5c400a5cd36282f
svroonland <svroonland@users.noreply.github.com>
2019-11-22 17:20:01 +0100

    Some method docs (#86)

--- src/main/scala/zio/kafka/client/Subscription.scala
+++ src/main/scala/zio/kafka/client/Subscription.scala
@@ -9,15 +9,37 @@ object Subscription {
   case class Topics(topics: Set[String]) extends Subscription
   case class Pattern(pattern: Regex)     extends Subscription
 
+  /**
+   * Create a subscription for one or more topics
+   *
+   * @param name Topic to subscribe to
+   * @param names Additional topics to subscribe to
+   */
   def topics(name: String, names: String*): Subscription =
     Topics(Set(name) ++ names.toSet)
 
+  /**
+   * Create a subscription for all topics matching the given pattern
+   *
+   * @param pattern Pattern
+   */
   def pattern(pattern: Regex): Subscription =
     Pattern(pattern)
 
+  /**
+   * Create a subscription for all topics matching the given pattern
+   *
+   * @param pattern Pattern to be converted to a Regex
+   * @return The created subscription or failure when the pattern is invalid
+   */
   def pattern(pattern: String): Task[Subscription] =
     Task.effect(new Regex(pattern)).map(Pattern)
 
+  /**
+   * Create a subscription for all topics matching the given pattern
+   *
+   * @param pattern Pattern
+   */
   def pattern(pattern: JPattern): Subscription =
     Pattern(new Regex(pattern.pattern()))
 }
