b09e56fcbb57f7ae57746150aa2ee614ecdc6d64
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2020-03-03 09:14:45 +0100

    Update Producer layer API after reviews

--- src/main/scala/zio/kafka/client/Producer.scala
+++ src/main/scala/zio/kafka/client/Producer.scala
@@ -27,7 +27,7 @@ object Producer {
      * but enqueue a batch of records and await all of their acknowledgements at once. That
      * amortizes the cost of sending requests to Kafka and increases throughput.
      */
-    def produce(record: ProducerRecord[K, V]): RIO[Blocking with R, Task[RecordMetadata]]
+    def produce(record: ProducerRecord[K, V]): RIO[R with Blocking, Task[RecordMetadata]]
 
     /**
      * Produces a chunk of records record. The effect returned from this method has two layers
@@ -43,17 +43,23 @@ object Producer {
      */
     def produceChunk(records: Chunk[ProducerRecord[K, V]]): RIO[R with Blocking, Task[Chunk[RecordMetadata]]]
 
+    /**
+     * Flushes the producer's internal buffer. This will guarantee that all records
+     * currently buffered will be transmitted to the broker.
+     */
+    def flush: BlockingTask[Unit]
+
     final def stream: ZSink[R with Blocking, Throwable, Nothing, Chunk[ProducerRecord[K, V]], Unit] =
       ZSink.drain.contramapM(produceChunk)
   }
 
-  class Live[R, K, V](
+  final case class Live[R, K, V](
     p: KafkaProducer[Array[Byte], Array[Byte]],
+    producerSettings: ProducerSettings,
     keySerializer: Serializer[R, K],
     valueSerializer: Serializer[R, V]
   ) extends Service[R, K, V] {
-
-    override def produce(record: ProducerRecord[K, V]): RIO[Blocking with R, Task[RecordMetadata]] =
+    override def produce(record: ProducerRecord[K, V]): RIO[R with Blocking, Task[RecordMetadata]] =
       for {
         done             <- Promise.make[Throwable, RecordMetadata]
         serializedRecord <- serialize(record)
@@ -109,42 +115,49 @@ object Producer {
         } yield done.await
       }
 
-    /**
-     * Flushes the producer's internal buffer. This will guarantee that all records
-     * currently buffered will be transmitted to the broker.
-     */
-    def flush: BlockingTask[Unit] = effectBlocking(p.flush())
+    override def flush: BlockingTask[Unit] = effectBlocking(p.flush())
 
     private def serialize(r: ProducerRecord[K, V]): RIO[R, ByteArrayProducerRecord] =
       for {
         key   <- keySerializer.serialize(r.topic, r.headers, r.key())
         value <- valueSerializer.serialize(r.topic, r.headers, r.value())
       } yield new ProducerRecord(r.topic, r.partition(), r.timestamp(), key, value, r.headers)
+
+    private[client] def close: UIO[Unit] = UIO(p.close(producerSettings.closeTimeout.asJava))
   }
 
-  private def createLive[R, K, V](
-    p: KafkaProducer[Array[Byte], Array[Byte]],
-    keySerializer: Serializer[R, K],
-    valueSerializer: Serializer[R, V]
-  ): Live[R, K, V] = new Live(p, keySerializer, valueSerializer)
+  def live[R, K, V](
+    implicit ts: Tagged[Serializer[R, K]],
+    td: Tagged[Serializer[R, V]],
+    tsv: Tagged[Service[R, K, V]]
+  ): ZLayer[Has[Serializer[R, K]] with Has[Serializer[R, V]] with Has[ProducerSettings], Throwable, Producer[R, K, V]] =
+    ZLayer.fromManaged {
+      val z = ZIO
+        .accessM[Has[Serializer[R, K]] with Has[Serializer[R, V]] with Has[ProducerSettings]] { env =>
+          val settings = env.get[ProducerSettings]
+          ZIO {
+            val props = settings.driverSettings.asJava
+            val rawProducer = new KafkaProducer[Array[Byte], Array[Byte]](
+              props,
+              new ByteArraySerializer(),
+              new ByteArraySerializer()
+            )
+            Live(rawProducer, settings, env.get[Serializer[R, K]], env.get[Serializer[R, V]])
+          }
+        }
+      z.toManaged(_.close)
+    }
 
-  def producer[R, K, V](
+  def make[R, K, V](
     settings: ProducerSettings,
     keySerializer: Serializer[R, K],
     valueSerializer: Serializer[R, V]
-  )(implicit tagged: Tagged[Service[R, K, V]]): ZLayer.NoDeps[Throwable, Producer[R, K, V]] = {
-    val p: Task[KafkaProducer[Array[Byte], Array[Byte]]] = ZIO {
-      val props = settings.driverSettings.asJava
-      new KafkaProducer[Array[Byte], Array[Byte]](
-        props,
-        new ByteArraySerializer(),
-        new ByteArraySerializer()
-      )
-    }
-
-    ZLayer.fromManaged(
-      p.toManaged(p => UIO(p.close(settings.closeTimeout.asJava)))
-        .map(producer => createLive(producer, keySerializer, valueSerializer))
-    )
-  }
+  )(
+    implicit ts: Tagged[Serializer[R, K]],
+    td: Tagged[Serializer[R, V]],
+    tsh: Tagged[Has[Serializer[R, K]]],
+    trv: Tagged[Has[Serializer[R, V]]],
+    tsv: Tagged[Service[R, K, V]]
+  ): ZLayer.NoDeps[Throwable, Producer[R, K, V]] =
+    (ZLayer.succeed(settings) ++ ZLayer.succeed(keySerializer) ++ ZLayer.succeed(valueSerializer)) >>> live[R, K, V]
 }
