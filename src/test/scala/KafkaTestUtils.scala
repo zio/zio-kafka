703324232b2008a88d9375f16f027e9cb946845f
Itamar Ravid <iravid@users.noreply.github.com>
2020-03-09 17:36:25 +0200

    Runloop terminates prematurely (#136)
    * Refactor tests to use the new accessors
    
    * Work around bug in new supervision model that interrupts Runloop
    
    * Decrease timeout

--- src/test/scala/KafkaTestUtils.scala
+++ src/test/scala/KafkaTestUtils.scala
@@ -17,83 +17,86 @@ import zio.kafka.serde.{ Deserializer, Serde, Serializer }
 import zio.kafka.producer._
 
 object KafkaTestUtils {
-  def producerSettings: ZIO[Kafka, Nothing, ProducerSettings] =
-    ZIO.access[Kafka](_.get[Kafka.Service].bootstrapServers).map(ProducerSettings(_))
-
   type StringProducer = Producer[Any, String, String]
   type StringConsumer = Consumer[Any, String, String]
 
-  val testProducer: ZLayer[Kafka, Throwable, StringProducer] =
+  val producerSettings: ZIO[Kafka, Nothing, ProducerSettings] =
+    ZIO.access[Kafka](_.get[Kafka.Service].bootstrapServers).map(ProducerSettings(_))
+
+  val stringProducer: ZLayer[Kafka, Throwable, StringProducer] =
     (ZLayer.fromEffect(producerSettings) ++ ZLayer.succeed(Serde.string: Serializer[Any, String])) >>>
       Producer.live[Any, String, String]
 
-  def withProducerStrings[R, A](
-    r: Producer.Service[Any, String, String] => RIO[R, A]
-  ): ZIO[R with StringProducer, Throwable, A] =
-    ZIO.accessM(env => r(env.get))
+  def produceOne(
+    topic: String,
+    key: String,
+    message: String
+  ): ZIO[Blocking with StringProducer, Throwable, RecordMetadata] =
+    Producer.produce[Any, String, String](new ProducerRecord(topic, key, message)).flatten
 
-  def produceOne(t: String, k: String, m: String): ZIO[Blocking with StringProducer, Throwable, RecordMetadata] =
-    Producer.produce[Any, String, String](new ProducerRecord(t, k, m)).flatten
+  def produceMany(
+    topic: String,
+    partition: Int,
+    kvs: Iterable[(String, String)]
+  ): ZIO[Blocking with StringProducer, Throwable, Chunk[RecordMetadata]] =
+    Producer
+      .produceChunk[Any, String, String](Chunk.fromIterable(kvs.map {
+        case (k, v) => new ProducerRecord(topic, partition, null, k, v)
+      }))
+      .flatten
 
-  def produceMany(t: String, kvs: Iterable[(String, String)]) =
-    withProducerStrings { p =>
-      val records = kvs.map {
-        case (k, v) => new ProducerRecord[String, String](t, k, v)
-      }
-      val chunk = Chunk.fromIterable(records)
-      p.produceChunk(chunk)
-    }.flatten
-
-  def produceMany(topic: String, partition: Int, kvs: Iterable[(String, String)]) =
-    withProducerStrings { p =>
-      val records = kvs.map {
-        case (k, v) => new ProducerRecord[String, String](topic, partition, null, k, v)
-      }
-      val chunk = Chunk.fromIterable(records)
-      p.produceChunk(chunk)
-    }.flatten
+  def produceMany(
+    topic: String,
+    kvs: Iterable[(String, String)]
+  ): ZIO[Blocking with StringProducer, Throwable, Chunk[RecordMetadata]] =
+    Producer
+      .produceChunk[Any, String, String](Chunk.fromIterable(kvs.map {
+        case (k, v) => new ProducerRecord(topic, k, v)
+      }))
+      .flatten
 
   def consumerSettings(groupId: String, clientId: String, offsetRetrieval: OffsetRetrieval = OffsetRetrieval.Auto()) =
-    ZIO
-      .access[Kafka](_.get[Kafka.Service].bootstrapServers)
-      .map(
-        ConsumerSettings(_)
-          .withGroupId(groupId)
-          .withClientId(clientId)
-          .withCloseTimeout(5.seconds)
-          .withProperties(
-            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG     -> "earliest",
-            ConsumerConfig.METADATA_MAX_AGE_CONFIG      -> "100",
-            ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG    -> "1000",
-            ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG -> "250",
-            ConsumerConfig.MAX_POLL_RECORDS_CONFIG      -> "10"
-          )
-          .withPerPartitionChunkPrefetch(16)
-          .withOffsetRetrieval(offsetRetrieval)
-      )
+    ZIO.access[Kafka] { kafka: Kafka =>
+      ConsumerSettings(kafka.get.bootstrapServers)
+        .withGroupId(groupId)
+        .withClientId(clientId)
+        .withCloseTimeout(5.seconds)
+        .withProperties(
+          ConsumerConfig.AUTO_OFFSET_RESET_CONFIG     -> "earliest",
+          ConsumerConfig.METADATA_MAX_AGE_CONFIG      -> "100",
+          ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG    -> "1000",
+          ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG -> "250",
+          ConsumerConfig.MAX_POLL_RECORDS_CONFIG      -> "10"
+        )
+        .withPerPartitionChunkPrefetch(16)
+        .withOffsetRetrieval(offsetRetrieval)
+    }
 
-  def withConsumerStrings[RC, A](
+  def stringConsumer(
     groupId: String,
     clientId: String,
-    diagnostics: Diagnostics = Diagnostics.NoOp,
-    offsetRetrieval: OffsetRetrieval = OffsetRetrieval.Auto()
-  )(
-    r: Consumer.Service[Any, String, String] => RIO[RC, A]
-  ): ZIO[RC with Clock with Blocking with Kafka, Throwable, A] = {
-    val layers = (ZLayer.requires[Clock] ++ ZLayer
-      .requires[Blocking] ++ ZLayer.succeed(diagnostics) ++
-      ZLayer.succeed(Serde.string: Deserializer[Any, String]) ++ consumerSettings(groupId, clientId, offsetRetrieval).toLayer) >>> Consumer.live
-    layers.build.use { consumer =>
-      r(consumer.get)
-    }
-  }
+    offsetRetrieval: OffsetRetrieval = OffsetRetrieval.Auto(),
+    diagnostics: Diagnostics = Diagnostics.NoOp
+  ): ZLayer[Kafka with Clock with Blocking, Throwable, Consumer[Any, String, String]] =
+    (ZLayer.fromEffect(consumerSettings(groupId, clientId, offsetRetrieval)) ++
+      ZLayer.requires[Clock] ++
+      ZLayer.requires[Blocking] ++
+      ZLayer.succeed(Deserializer.string: Deserializer[Any, String]) ++
+      ZLayer.succeed(diagnostics)) >>> Consumer.live[Any, String, String]
 
   def consumeWithStrings[RC](groupId: String, clientId: String, subscription: Subscription)(
     r: (String, String) => URIO[RC, Unit]
   ): RIO[RC with Blocking with Clock with Kafka, Unit] =
-    withConsumerStrings(groupId, clientId)(_.consumeWith(subscription)(r))
+    consumerSettings(groupId, clientId).flatMap { settings =>
+      Consumer.consumeWith(
+        settings,
+        subscription,
+        Deserializer.string,
+        Deserializer.string
+      )(r)
+    }
 
-  def adminSettings =
+  def adminSettings: ZIO[Kafka, Nothing, AdminClientSettings] =
     ZIO.access[Kafka](_.get[Kafka.Service].bootstrapServers).map(AdminClientSettings(_))
 
   def withAdmin[T](f: AdminClient => RIO[Clock with Kafka with Blocking, T]) =
@@ -106,14 +109,11 @@ object KafkaTestUtils {
                }
                .provideSomeLayer[Kafka](Clock.live ++ Blocking.live)
     } yield fRes
+  def randomThing(prefix: String): Task[String] =
+    Task(UUID.randomUUID()).map(uuid => s"$prefix-$uuid")
 
-  def randomThing(prefix: String) =
-    for {
-      l <- Task(UUID.randomUUID())
-    } yield s"$prefix-$l"
+  def randomTopic: Task[String] = randomThing("topic")
 
-  def randomTopic = randomThing("topic")
-
-  def randomGroup = randomThing("group")
+  def randomGroup: Task[String] = randomThing("group")
 
 }
