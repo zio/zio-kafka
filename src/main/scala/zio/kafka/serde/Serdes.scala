f194e81c3b80044668f8be0a03c6c18902d81f12
Erik Gast <egast@users.noreply.github.com>
2020-05-17 12:40:14 +0200

    Make predefined serdes in Serdes lazy vals to prevent dead-lock in test

--- src/main/scala/zio/kafka/serde/Serdes.scala
+++ src/main/scala/zio/kafka/serde/Serdes.scala
@@ -6,13 +6,13 @@ import java.util.UUID
 import org.apache.kafka.common.serialization.{ Serdes => KafkaSerdes }
 
 private[zio] trait Serdes {
-  val long: Serde[Any, Long]             = Serde(KafkaSerdes.Long()).inmap(Long2long)(long2Long)
-  val int: Serde[Any, Int]               = Serde(KafkaSerdes.Integer()).inmap(Integer2int)(int2Integer)
-  val short: Serde[Any, Short]           = Serde(KafkaSerdes.Short()).inmap(Short2short)(short2Short)
-  val float: Serde[Any, Float]           = Serde(KafkaSerdes.Float()).inmap(Float2float)(float2Float)
-  val double: Serde[Any, Double]         = Serde(KafkaSerdes.Double()).inmap(Double2double)(double2Double)
-  val string: Serde[Any, String]         = Serde(KafkaSerdes.String())
-  val byteArray: Serde[Any, Array[Byte]] = Serde(KafkaSerdes.ByteArray())
-  val byteBuffer: Serde[Any, ByteBuffer] = Serde(KafkaSerdes.ByteBuffer())
-  val uuid: Serde[Any, UUID]             = Serde(KafkaSerdes.UUID())
+  lazy val long: Serde[Any, Long]             = Serde(KafkaSerdes.Long()).inmap(Long2long)(long2Long)
+  lazy val int: Serde[Any, Int]               = Serde(KafkaSerdes.Integer()).inmap(Integer2int)(int2Integer)
+  lazy val short: Serde[Any, Short]           = Serde(KafkaSerdes.Short()).inmap(Short2short)(short2Short)
+  lazy val float: Serde[Any, Float]           = Serde(KafkaSerdes.Float()).inmap(Float2float)(float2Float)
+  lazy val double: Serde[Any, Double]         = Serde(KafkaSerdes.Double()).inmap(Double2double)(double2Double)
+  lazy val string: Serde[Any, String]         = Serde(KafkaSerdes.String())
+  lazy val byteArray: Serde[Any, Array[Byte]] = Serde(KafkaSerdes.ByteArray())
+  lazy val byteBuffer: Serde[Any, ByteBuffer] = Serde(KafkaSerdes.ByteBuffer())
+  lazy val uuid: Serde[Any, UUID]             = Serde(KafkaSerdes.UUID())
 }
