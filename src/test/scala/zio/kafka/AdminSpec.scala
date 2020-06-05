359abe338a846f39f3ba124a25237844969ff16e
Erik Gast <egast@users.noreply.github.com>
2020-06-05 17:55:55 +0200

    Add describeConfigs to AdminClient (#168)
    Co-authored-by: Itamar Ravid <iravid@users.noreply.github.com>

--- src/test/scala/zio/kafka/AdminSpec.scala
+++ src/test/scala/zio/kafka/AdminSpec.scala
@@ -1,10 +1,11 @@
 package zio.kafka.admin
 
+import org.apache.kafka.common.config.ConfigResource
 import zio.kafka.KafkaTestUtils
 import zio.kafka.embedded.Kafka
-import zio.test._
 import zio.test.Assertion._
 import zio.test.TestAspect._
+import zio.test._
 import zio.test.environment.TestEnvironment
 
 object AdminSpec extends DefaultRunnableSpec {
@@ -63,6 +64,36 @@ object AdminSpec extends DefaultRunnableSpec {
               assert(descriptions.size)(equalTo(2)) &&
               assert(list3.size)(equalTo(0))
 
+        }
+      },
+      testM("create, describe topic config, delete multiple topic") {
+        KafkaTestUtils.withAdmin {
+          client =>
+            for {
+              list1 <- client.listTopics()
+              _     <- client.createTopics(List(AdminClient.NewTopic("topic6", 1, 1), AdminClient.NewTopic("topic7", 4, 1)))
+              configs <- client.describeConfigs(
+                          List(
+                            new ConfigResource(ConfigResource.Type.TOPIC, "topic6"),
+                            new ConfigResource(ConfigResource.Type.TOPIC, "topic7")
+                          )
+                        )
+              _     <- client.deleteTopics(List("topic6", "topic7"))
+              list3 <- client.listTopics()
+            } yield assert(list1.size)(equalTo(0)) &&
+              assert(configs.size)(equalTo(2)) &&
+              assert(list3.size)(equalTo(0))
+        }
+      },
+      testM("describe broker config") {
+        KafkaTestUtils.withAdmin { client =>
+          for {
+            configs <- client.describeConfigs(
+                        List(
+                          new ConfigResource(ConfigResource.Type.BROKER, "0")
+                        )
+                      )
+          } yield assert(configs.size)(equalTo(1))
         }
       }
     ).provideSomeLayerShared[TestEnvironment](Kafka.embedded.mapError(TestFailure.fail)) @@ sequential
