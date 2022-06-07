9a2745821d30ff4cb48c8ed91db18009ca82d358
svroonland <svroonland@users.noreply.github.com>
2022-06-07 09:42:25 +0200

    Update PartitionStreamControl.scala

--- src/main/scala/zio/kafka/consumer/internal/PartitionStreamControl.scala
+++ src/main/scala/zio/kafka/consumer/internal/PartitionStreamControl.scala
@@ -4,7 +4,7 @@ import zio._
 import zio.kafka.consumer.internal.Runloop.ByteArrayCommittableRecord
 import zio.stream.Take
 
-case class PartitionStreamControl(
+private case class PartitionStreamControl(
   interrupt: Promise[Throwable, Unit],
   drainQueue: Queue[Take[Nothing, ByteArrayCommittableRecord]]
 ) {
