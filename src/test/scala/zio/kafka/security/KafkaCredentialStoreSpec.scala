3b040c2ce303686601fce8179e371f4275d6c1ee
mhodovaniuk <mykhailo.hodovaniuk@gmail.com>
2022-07-02 20:07:30 +0300

    Fix formatting

--- src/test/scala/zio/kafka/security/KafkaCredentialStoreSpec.scala
+++ src/test/scala/zio/kafka/security/KafkaCredentialStoreSpec.scala
@@ -27,14 +27,18 @@ object KafkaCredentialStoreSpec extends ZIOSpecDefault {
 
         val store = KafkaCredentialStore.fromPemStrings(caCert, privateKey, clientCert)
 
-        assert(store.properties)(equalTo(Map(
-            "security.protocol" -> "SSL",
-            "ssl.truststore.type" -> "PEM",
-            "ssl.truststore.certificates" -> "-----BEGIN CERTIFICATE----- RmFrZSBDQSBjZXJ0aWZpY2F0ZSBGYWtlIENBIGNlcnRpZmljYXRlIEZha2UgQ0EgY2VydGlmaWNh dGUgRmFrZSBDQSBjZXJ0aWZpY2F0ZQ== -----END CERTIFICATE-----",
-            "ssl.keystore.type" -> "PEM",
-            "ssl.keystore.key" -> "-----BEGIN PRIVATE KEY----- RmFrZSBwcml2YXRlIGtleSBGYWtlIHByaXZhdGUga2V5IEZha2UgcHJpdmF0ZSBrZXkgRmFrZSBw cml2YXRlIGtleSBGYWtlIHByaXZhdGUga2V5IA== -----END PRIVATE KEY-----",
-            "ssl.keystore.certificate.chain" -> "-----BEGIN CERTIFICATE----- RmFrZSBjbGllbnQgY2VydCBGYWtlIGNsaWVudCBjZXJ0IEZha2UgY2xpZW50IGNlcnQgRmFrZSBj bGllbnQgY2VydCBGYWtlIGNsaWVudCBjZXJ0IA== -----END CERTIFICATE-----"
-          )))
+        assert(store.properties)(
+          equalTo(
+            Map(
+              "security.protocol"   -> "SSL",
+              "ssl.truststore.type" -> "PEM",
+              "ssl.truststore.certificates" -> "-----BEGIN CERTIFICATE----- RmFrZSBDQSBjZXJ0aWZpY2F0ZSBGYWtlIENBIGNlcnRpZmljYXRlIEZha2UgQ0EgY2VydGlmaWNh dGUgRmFrZSBDQSBjZXJ0aWZpY2F0ZQ== -----END CERTIFICATE-----",
+              "ssl.keystore.type" -> "PEM",
+              "ssl.keystore.key" -> "-----BEGIN PRIVATE KEY----- RmFrZSBwcml2YXRlIGtleSBGYWtlIHByaXZhdGUga2V5IEZha2UgcHJpdmF0ZSBrZXkgRmFrZSBw cml2YXRlIGtleSBGYWtlIHByaXZhdGUga2V5IA== -----END PRIVATE KEY-----",
+              "ssl.keystore.certificate.chain" -> "-----BEGIN CERTIFICATE----- RmFrZSBjbGllbnQgY2VydCBGYWtlIGNsaWVudCBjZXJ0IEZha2UgY2xpZW50IGNlcnQgRmFrZSBj bGllbnQgY2VydCBGYWtlIGNsaWVudCBjZXJ0IA== -----END CERTIFICATE-----"
+            )
+          )
+        )
       }
     )
   )
