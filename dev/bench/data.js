130f7b9e7445e8b39e778107dc4ec3e8251fa1f3
github-action-benchmark <github@users.noreply.github.com>
2023-01-30 02:19:26 +0000

    add JMH Benchmark (jmh) benchmark result for 22152cb7614e2bfa373a9fcfe82e37bae521f49a

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1675045166947,
+  "repoUrl": "https://github.com/zio/zio-kafka",
+  "entries": {
+    "JMH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "guizmaii@users.noreply.github.com",
+            "name": "Jules Ivanic",
+            "username": "guizmaii"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "22152cb7614e2bfa373a9fcfe82e37bae521f49a",
+          "message": "Fix Benchmark CI configuration (#617)",
+          "timestamp": "2023-01-30T10:16:35+08:00",
+          "tree_id": "8501583f50f442e6ec80646ce221b7b5db6c21cf",
+          "url": "https://github.com/zio/zio-kafka/commit/22152cb7614e2bfa373a9fcfe82e37bae521f49a"
+        },
+        "date": 1675045166311,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "zio.kafka.bench.JMHSample_01_HelloWorld.wellHelloThere",
+            "value": 1989580332.5496168,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 5\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
