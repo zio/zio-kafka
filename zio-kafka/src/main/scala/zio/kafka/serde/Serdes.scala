7d629c0bdfdaee1fe894f36af3c72dea39034ac1
svroonland <svroonland@users.noreply.github.com>
2023-02-27 09:59:00 +0100

    Fix docs (#672)

--- zio-kafka/src/main/scala/zio/kafka/serde/Serdes.scala
+++ zio-kafka/src/main/scala/zio/kafka/serde/Serdes.scala
@@ -15,10 +15,26 @@ private[zio] trait Serdes {
   lazy val float: Serde[Any, Float]   = convertPrimitiveSerde(KafkaSerdes.Float()).inmap(Float2float)(float2Float)
   lazy val double: Serde[Any, Double] = convertPrimitiveSerde(KafkaSerdes.Double()).inmap(Double2double)(double2Double)
   lazy val string: Serde[Any, String] = convertPrimitiveSerde(KafkaSerdes.String())
-  lazy val byteArray: Serde[Any, Array[Byte]] = convertPrimitiveSerde(KafkaSerdes.ByteArray())
   lazy val bytes: Serde[Any, Bytes]           = convertPrimitiveSerde(KafkaSerdes.Bytes())
   lazy val byteBuffer: Serde[Any, ByteBuffer] = convertPrimitiveSerde(KafkaSerdes.ByteBuffer())
   lazy val uuid: Serde[Any, UUID]             = convertPrimitiveSerde(KafkaSerdes.UUID())
+
+  /**
+   * Optimisation
+   *
+   * Here, we're not using `KafkaSerdes.ByteArray()` because the underlying deserializer and serializer implementations
+   * are just identity functions.
+   *
+   * That allows us to use [[ZIO.succeed]] instead of [[ZIO.attempt]].
+   */
+  lazy val byteArray: Serde[Any, Array[Byte]] =
+    new Serde[Any, Array[Byte]] {
+      override final def serialize(topic: String, headers: Headers, value: Array[Byte]): RIO[Any, Array[Byte]] =
+        ZIO.succeed(value)
+
+      override final def deserialize(topic: String, headers: Headers, data: Array[Byte]): RIO[Any, Array[Byte]] =
+        ZIO.succeed(data)
+    }
 
   private[this] def convertPrimitiveSerde[T](serde: KafkaSerde[T]): Serde[Any, T] =
     new Serde[Any, T] {
