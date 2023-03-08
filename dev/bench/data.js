e911f64f1ae1bb8e515b9970411630c8959eb626
github-action-benchmark <github@users.noreply.github.com>
2023-03-08 10:03:44 +0000

    add JMH Benchmark (jmh) benchmark result for 7ecfd9f875ea7ff35d1c69787c5ff379514ddeab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678269486118,
+  "lastUpdate": 1678269824880,
   "repoUrl": "https://github.com/zio/zio-kafka",
   "entries": {
     "JMH Benchmark": [
@@ -1648,6 +1648,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "zio.kafka.bench.ConsumerBenchmark.throughputWithCommits",
             "value": 1000.3645129333332,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 5\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7ecfd9f875ea7ff35d1c69787c5ff379514ddeab",
+          "message": "Update README.md (#693)\n\nCo-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2023-03-08T10:57:06+01:00",
+          "tree_id": "ef3b91368596de9f91a3f2bfa354001a29249002",
+          "url": "https://github.com/zio/zio-kafka/commit/7ecfd9f875ea7ff35d1c69787c5ff379514ddeab"
+        },
+        "date": 1678269824525,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "zio.kafka.bench.ConsumerBenchmark.throughput",
+            "value": 802.2134881666665,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "zio.kafka.bench.ConsumerBenchmark.throughputWithCommits",
+            "value": 968.4077337333333,
             "unit": "ms/op",
             "extra": "iterations: 3\nforks: 5\nthreads: 1"
           }
