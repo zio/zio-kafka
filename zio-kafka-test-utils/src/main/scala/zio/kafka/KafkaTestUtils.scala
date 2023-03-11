bdf325d67716bfb8020b273703d9c241743a6f88
jules Ivanic <jules.ivanic@gmail.com>
2023-03-11 13:39:52 +0800

    MAX_POLL_RECORDS_CONFIG         -> "1000"

--- zio-kafka-test-utils/src/main/scala/zio/kafka/KafkaTestUtils.scala
+++ zio-kafka-test-utils/src/main/scala/zio/kafka/KafkaTestUtils.scala
@@ -81,7 +81,7 @@ object KafkaTestUtils {
           ConsumerConfig.METADATA_MAX_AGE_CONFIG         -> "100",
           ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG       -> "3000",
           ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG    -> "250",
-          ConsumerConfig.MAX_POLL_RECORDS_CONFIG         -> "10",
+          ConsumerConfig.MAX_POLL_RECORDS_CONFIG         -> "1000",
           ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG -> allowAutoCreateTopics.toString
         )
         .withPerPartitionChunkPrefetch(16)
