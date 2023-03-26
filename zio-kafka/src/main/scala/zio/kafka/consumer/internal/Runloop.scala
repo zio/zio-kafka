72958fca496f4b9d817826ad42901c4e7fc0457b
jules Ivanic <jules.ivanic@gmail.com>
2023-03-26 23:45:23 +0200

    Keep chunking of commands (inlined version)

--- zio-kafka/src/main/scala/zio/kafka/consumer/internal/Runloop.scala
+++ zio-kafka/src/main/scala/zio/kafka/consumer/internal/Runloop.scala
@@ -491,48 +491,42 @@ private[consumer] final class Runloop private (
           s"Starting poll with ${state.pendingRequests.size} pending requests and ${state.pendingCommits.size} pending commits"
         )
 
-    ZStream
-      .fromQueue(commandQueue)
-      .timeoutFail[Throwable](RunloopTimeout)(runloopTimeout)
-      .takeWhile(_ != StopRunloop)
-      .runChunksFoldZIO(State.initial) { case (state, commands) =>
+    val sink: ZSink[Any, Throwable, Command, Nothing, State] = {
+      def consume(state: State, commands: Chunk[Command]): ZIO[Any, Throwable, State] =
         for {
           _          <- ZIO.logTrace(s"Processing ${commands.size} commands: ${commands.mkString(",")}")
           isShutdown <- isShutdown
           handleCommand = if (isShutdown) handleShutdown _ else handleOperational _
           updatedState <- ZIO.foldLeft(commands)(state)(handleCommand)
 
-          updatedStateAfterPoll <- if (updatedState.shouldPoll)
-                                     logPollStart(updatedState) *> handlePoll(updatedState)
+          updatedStateAfterPoll <- if (updatedState.shouldPoll) logPollStart(updatedState) *> handlePoll(updatedState)
                                    else ZIO.succeed(updatedState)
           // Immediately poll again, after processing all new queued commands
-          _ <- commandQueue.offer(Command.Poll).when(updatedStateAfterPoll.shouldPoll)
+          // noinspection SimplifyWhenInspection
+          _ <- if (updatedStateAfterPoll.shouldPoll) commandQueue.offer(Command.Poll) else ZIO.unit
         } yield updatedStateAfterPoll
-      }
+
+      def reader(s: State): ZChannel[Any, Throwable, Chunk[Command], Any, Throwable, Nothing, State] =
+        ZChannel.readWith(
+          (in: Chunk[Command]) => ZChannel.fromZIO(consume(s, in)).flatMap(reader),
+          (err: Throwable) => ZChannel.fail(err),
+          (_: Any) => ZChannel.succeedNow(s)
+        )
+
+      ZSink.fromChannel(reader(State.initial))
+    }
+
+    ZStream
+      .fromQueue(commandQueue)
+      .timeoutFail[Throwable](RunloopTimeout)(runloopTimeout)
+      .takeWhile(_ != StopRunloop)
+      .run(sink)
       .tapErrorCause(cause => ZIO.logErrorCause("Error in Runloop", cause))
       .onError(cause => partitions.offer(Take.failCause(cause)))
   }
 }
 
 private[consumer] object Runloop {
-  private implicit final class RichZStream[R, E, A](private val stream: ZStream[R, E, A]) extends AnyVal {
-
-    /**
-     * Adapted from [[ZStream.runFoldZIO]]
-     */
-    def runChunksFoldZIO[R1 <: R, E1 >: E, S](s: => S)(f: (S, Chunk[A]) => ZIO[R1, E1, S])(implicit
-      trace: Trace
-    ): ZIO[R1, E1, S] = ZIO.scoped[R1](runChunksFoldWhileScopedZIO[R1, E1, S](s)(_ => true)(f))
-
-    /**
-     * Adapted from [[ZStream.runFoldWhileScopedZIO]]
-     */
-    private def runChunksFoldWhileScopedZIO[R1 <: R, E1 >: E, S](
-      s: => S
-    )(cont: S => Boolean)(f: (S, Chunk[A]) => ZIO[R1, E1, S])(implicit trace: Trace): ZIO[R1 with Scope, E1, S] =
-      stream.runScoped(ZSink.foldChunksZIO(s)(cont)(f))
-  }
-
   type ByteArrayCommittableRecord = CommittableRecord[Array[Byte], Array[Byte]]
 
   // Internal parameters, should not be necessary to tune
