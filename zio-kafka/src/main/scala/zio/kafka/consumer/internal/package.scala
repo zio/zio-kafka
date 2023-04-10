8031d1a0f9f47acd88e1129d584cba1049cb7bac
Erik van Oosten <e.vanoosten@grons.nl>
2023-04-10 17:17:48 +0200

    Properly attribute SameThreadRuntimeLayer to @adamgfraser

--- zio-kafka/src/main/scala/zio/kafka/consumer/internal/package.scala
+++ zio-kafka/src/main/scala/zio/kafka/consumer/internal/package.scala
@@ -5,6 +5,12 @@ import zio.internal.ExecutionMetrics
 
 package object internal {
 
+  /**
+   * A runtime layer that can be used to run everything on the thread of the caller.
+   *
+   * Provided by Adam Fraser in Discord:
+   * https://discord.com/channels/629491597070827530/630498701860929559/1094279123880386590
+   */
   private[internal] val SameThreadRuntimeLayer: ZLayer[Any, Nothing, Unit] = {
     val sameThreadExecutor = new Executor() {
       override def metrics(implicit unsafe: Unsafe): Option[ExecutionMetrics] = None
