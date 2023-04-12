005a5c5efa50a79bb62cb7e42e17dfcdec4a9759
Jules Ivanic <guizmaii@users.noreply.github.com>
2023-04-12 15:55:52 +0200

    Run the Runloop on a dedicated single-threaded thread pool (#802)

---
+++ zio-kafka/src/main/scala/zio/kafka/consumer/internal/RunloopExecutor.scala
@@ -0,0 +1,24 @@
+package zio.kafka.consumer.internal
+
+import zio.{ Executor, Scope, ZIO }
+
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicLong
+
+private[consumer] object RunloopExecutor {
+
+  private val counter: AtomicLong = new AtomicLong(0)
+
+  private def newSingleThreadedExecutor(i: Long): ZIO[Scope, Throwable, Executor] =
+    ZIO.acquireRelease {
+      ZIO.attempt {
+        val javaExecutor =
+          Executors.newSingleThreadExecutor(runnable => new Thread(runnable, s"zio-kafka-runloop-thread-$i"))
+
+        Executor.fromJavaExecutor(javaExecutor) -> javaExecutor
+      }
+    } { case (_, executor) => ZIO.attempt(executor.shutdown()).orDie }.map(_._1)
+
+  val newInstance: ZIO[Scope, Throwable, Executor] = newSingleThreadedExecutor(counter.getAndIncrement())
+
+}
