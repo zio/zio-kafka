2ce416ad6c68915afb2ffa9b98b4726cedecaa76
jules Ivanic <jules.ivanic@gmail.com>
2023-04-13 10:57:30 +0200

    tata

--- build.sbt
+++ build.sbt
@@ -164,9 +164,10 @@ lazy val zioKafkaExample =
     .settings(run / fork := false)
     .settings(
       libraryDependencies ++= Seq(
-        "dev.zio"       %% "zio"             % "2.0.10",
-        "dev.zio"       %% "zio-kafka"       % "2.1.3",
-        "ch.qos.logback" % "logback-classic" % "1.4.6"
+        "dev.zio"                 %% "zio"             % "2.0.10",
+        "dev.zio"                 %% "zio-kafka"       % "2.2",
+        "ch.qos.logback"           % "logback-classic" % "1.4.6",
+        "io.github.embeddedkafka" %% "embedded-kafka"  % embeddedKafkaVersion
       )
     )
 
--- zio-kafka-example/src/main/scala/zio/kafka/example/Main.scala
+++ zio-kafka-example/src/main/scala/zio/kafka/example/Main.scala
@@ -1,23 +1,44 @@
 package zio.kafka.example
 
+import io.github.embeddedkafka.{ EmbeddedK, EmbeddedKafka, EmbeddedKafkaConfig }
 import zio._
 import zio.kafka.consumer.diagnostics.Diagnostics
 import zio.kafka.consumer.{ Consumer, ConsumerSettings, Subscription }
 import zio.kafka.serde.Serde
 
+trait Kafka {
+  def bootstrapServers: List[String]
+  def stop(): UIO[Unit]
+}
+
+object Kafka {
+  final case class EmbeddedKafkaService(embeddedK: EmbeddedK) extends Kafka {
+    override def bootstrapServers: List[String] = List(s"localhost:${embeddedK.config.kafkaPort}")
+    override def stop(): UIO[Unit]              = ZIO.succeed(embeddedK.stop(true))
+  }
+
+  val embedded: ZLayer[Any, Throwable, Kafka] = ZLayer.scoped {
+    implicit val embeddedKafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig(
+      customBrokerProperties = Map(
+        "group.min.session.timeout.ms"     -> "500",
+        "group.initial.rebalance.delay.ms" -> "0",
+        "authorizer.class.name"            -> "kafka.security.authorizer.AclAuthorizer",
+        "super.users"                      -> "User:ANONYMOUS"
+      )
+    )
+    ZIO.acquireRelease(ZIO.attempt(EmbeddedKafkaService(EmbeddedKafka.start())))(_.stop())
+  }
+}
+
 object Main extends ZIOAppDefault {
 
-  // TODO: Use your own server
-  private val boostrapServers = ???
+  private val topic = "test-topic"
 
-  private def consumerLayer: ZLayer[Any, Throwable, Consumer] = {
+  private def consumerLayer(kafka: Kafka): ZLayer[Any, Throwable, Consumer] = {
     val consumerSettings =
-      ConsumerSettings(boostrapServers)
+      ConsumerSettings(kafka.bootstrapServers)
         .withPollTimeout(500.millis)
         .withGroupId("test")
-        .withProperties(
-          "max.poll.records" -> "1000"
-        )
 
     ZLayer.make[Consumer](
       ZLayer.succeed(consumerSettings),
@@ -27,19 +48,18 @@ object Main extends ZIOAppDefault {
   }
 
   override def run: ZIO[ZIOAppArgs with Scope, Any, Any] =
-    ZIO.logLevel(LogLevel.Debug) {
-      ZIO.addFinalizer(ZIO.logInfo("Stopping app")) *>
-        (
-          for {
-            _ <- ZIO.logInfo(s"Starting app")
-            stream = Consumer
-                       .plainStream(Subscription.topics("wikipedia.parsed"), Serde.string, Serde.string)
-                       .provideLayer(consumerLayer)
-            _        <- ZIO.logInfo(s"Consuming messages...")
-            consumed <- stream.take(1000).runCount
-            _        <- ZIO.logInfo(s"Consumed $consumed records")
-          } yield ()
-        )
-    }
+    ZIO.addFinalizer(ZIO.logInfo("Stopping app")) *>
+      (
+        for {
+          _     <- ZIO.logInfo(s"Starting app")
+          kafka <- ZIO.service[Kafka]
+          stream = Consumer
+                     .plainStream(Subscription.topics(topic), Serde.string, Serde.string)
+                     .provideLayer(consumerLayer(kafka))
+          _        <- ZIO.logInfo(s"Consuming messages...")
+          consumed <- stream.take(1000).tap(r => ZIO.logInfo(s"Consumed record $r")).runCount
+          _        <- ZIO.logInfo(s"Consumed $consumed records")
+        } yield ()
+      ).provideSomeLayer[ZIOAppArgs with Scope](Kafka.embedded)
 
 }
