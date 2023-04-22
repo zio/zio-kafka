65b341b0a37586a2cb2a02abc85d73650c6b287c
jules Ivanic <jules.ivanic@gmail.com>
2023-04-22 14:49:49 +0200

    Put back and deprecated `ZIOSpecWithKafka`

---
+++ zio-kafka-test-utils/src/main/scala/zio/kafka/ZIOSpecWithKafka.scala
@@ -0,0 +1,31 @@
+package zio.kafka
+
+import zio.kafka.embedded.Kafka
+import zio.test._
+import zio.ZLayer
+
+@deprecated(
+  message =
+    """
+      |This trait is deprecated.
+      |
+      |The advised way to bring a Kafka instance in your tests is to use the zio-test `provideShared` or `provideSomeShared` methods on your suite:
+      |```
+      |val mySuite =
+      |  suite("my suite") {
+      |    test("my test") {
+      |      ...
+      |    }
+      |  }.provideShared(Kafka.embedded)
+      |```
+      |
+      |Look at the tests in the `zio-kafka-test` project for examples.
+      |""".stripMargin,
+  since = "2.3.0"
+)
+trait ZIOSpecWithKafka extends ZIOSpec[TestEnvironment with Kafka] {
+
+  override val bootstrap: ZLayer[Any, Any, TestEnvironment with Kafka] =
+    testEnvironment ++ Kafka.embedded
+
+}
