7188b660cf305c5d21f9e568751802146977a461
Vasileios Lampridis <basilis.lab@gmail.com>
2023-06-04 12:51:35 +0100

    [Testkit] Allow to customise broker properties (#893)

--- zio-kafka-testkit/src/main/scala/zio/kafka/testkit/Kafka.scala
+++ zio-kafka-testkit/src/main/scala/zio/kafka/testkit/Kafka.scala
@@ -4,11 +4,9 @@ import _root_.kafka.server.KafkaConfig
 import io.github.embeddedkafka.{ EmbeddedK, EmbeddedKafka, EmbeddedKafkaConfig }
 import zio._
 
-import java.util.concurrent.atomic.AtomicReference
-import scala.util.control.NonFatal
-
 trait Kafka {
   def bootstrapServers: List[String]
+
   def stop(): UIO[Unit]
 }
 
@@ -19,60 +17,67 @@ object Kafka {
   /**
    * Creates an in-memory Kafka instance with a random port.
    */
-  val embedded: ZLayer[Any, Throwable, Kafka] = ZLayer.scoped {
-    def embeddedKafkaConfig(kafkaPort: Int, zooKeeperPort: Int): EmbeddedKafkaConfig =
-      EmbeddedKafkaConfig(
-        kafkaPort = kafkaPort,
-        zooKeeperPort = zooKeeperPort,
-        customBrokerProperties = Map(
+  val embedded: ZLayer[Any, Throwable, Kafka] = embeddedWith(_ => Map.empty)
+
+  /**
+   * Creates an in-memory Kafka instance with a random port.
+   * @param customBrokerProps
+   *   add/update broker properties
+   */
+  def embeddedWith(customBrokerProps: Ports => Map[String, String]): ZLayer[Any, Throwable, Kafka] =
+    embeddedWithBrokerProps(
+      _ =>
+        Map(
           "group.min.session.timeout.ms"     -> "500",
           "group.initial.rebalance.delay.ms" -> "0",
           "authorizer.class.name"            -> "kafka.security.authorizer.AclAuthorizer",
           "super.users"                      -> "User:ANONYMOUS"
-        )
-      )
-
-    ZIO.acquireRelease(
-      startKafka(embeddedKafkaConfig).map(EmbeddedKafkaService.apply)
-    )(_.stop())
-  }
+        ),
+      customBrokerProps
+    )
 
   /**
    * Creates an in-memory Kafka instance with a random port and SASL authentication configured.
    */
-  val saslEmbedded: ZLayer[Any, Throwable, Kafka.Sasl] = ZLayer.scoped {
-    def embeddedKafkaConfig(kafkaPort: Int, zooKeeperPort: Int): EmbeddedKafkaConfig =
-      EmbeddedKafkaConfig(
-        kafkaPort = kafkaPort,
-        zooKeeperPort = zooKeeperPort,
-        customBrokerProperties = Map(
+  val saslEmbedded: ZLayer[Any, Throwable, Kafka.Sasl] = saslEmbeddedWith(_ => Map.empty)
+
+  /**
+   * Creates an in-memory Kafka instance with a random port and SASL authentication configured.
+   * @param customBrokerProps
+   *   add/update broker properties
+   */
+  def saslEmbeddedWith(customBrokerProps: Ports => Map[String, String]): ZLayer[Any, Throwable, Kafka.Sasl] =
+    embeddedWithBrokerProps(
+      ports =>
+        Map(
           "group.min.session.timeout.ms"         -> "500",
           "group.initial.rebalance.delay.ms"     -> "0",
           "authorizer.class.name"                -> "kafka.security.authorizer.AclAuthorizer",
           "sasl.enabled.mechanisms"              -> "PLAIN",
           "sasl.mechanism.inter.broker.protocol" -> "PLAIN",
           "inter.broker.listener.name"           -> "SASL_PLAINTEXT",
-          "listeners"                            -> s"SASL_PLAINTEXT://localhost:$kafkaPort",
-          "advertised.listeners"                 -> s"SASL_PLAINTEXT://localhost:$kafkaPort",
+          "listeners"                            -> s"SASL_PLAINTEXT://localhost:${ports.kafkaPort}",
+          "advertised.listeners"                 -> s"SASL_PLAINTEXT://localhost:${ports.kafkaPort}",
           "super.users"                          -> "User:admin",
           "listener.name.sasl_plaintext.plain.sasl.jaas.config" -> """org.apache.kafka.common.security.plain.PlainLoginModule required username="admin" password="admin-secret" user_admin="admin-secret" user_kafkabroker1="kafkabroker1-secret";"""
-        )
-      )
-
-    ZIO.acquireRelease(
-      startKafka(embeddedKafkaConfig).map(k => Kafka.Sasl(EmbeddedKafkaService(k)))
-    )(_.value.stop())
-  }
+        ),
+      customBrokerProps
+    ).project(Sasl(_))
 
   /**
    * Creates an in-memory Kafka instance with a random port and SSL authentication configured.
    */
-  val sslEmbedded: ZLayer[Any, Throwable, Kafka] = ZLayer.scoped {
-    def embeddedKafkaConfig(kafkaPort: Int, zooKeeperPort: Int): EmbeddedKafkaConfig =
-      EmbeddedKafkaConfig(
-        kafkaPort = kafkaPort,
-        zooKeeperPort = zooKeeperPort,
-        customBrokerProperties = Map(
+  val sslEmbedded: ZLayer[Any, Throwable, Kafka] = sslEmbeddedWith(_ => Map.empty)
+
+  /**
+   * reates an in-memory Kafka instance with a random port and SSL authentication configured.
+   * @param customBrokerProps
+   *   add/update broker properties
+   */
+  def sslEmbeddedWith(customBrokerProps: Ports => Map[String, String]): ZLayer[Any, Throwable, Kafka] =
+    embeddedWithBrokerProps(
+      ports =>
+        Map(
           "group.min.session.timeout.ms"          -> "500",
           "group.initial.rebalance.delay.ms"      -> "0",
           "authorizer.class.name"                 -> "kafka.security.authorizer.AclAuthorizer",
@@ -87,16 +92,12 @@ object Kafka {
           "ssl.keystore.password"                 -> "123456",
           "ssl.key.password"                      -> "123456",
           KafkaConfig.InterBrokerListenerNameProp -> "SSL",
-          KafkaConfig.ListenersProp               -> s"SSL://localhost:$kafkaPort",
-          KafkaConfig.AdvertisedListenersProp     -> s"SSL://localhost:$kafkaPort",
+          KafkaConfig.ListenersProp               -> s"SSL://localhost:${ports.kafkaPort}",
+          KafkaConfig.AdvertisedListenersProp     -> s"SSL://localhost:${ports.kafkaPort}",
           KafkaConfig.ZkConnectionTimeoutMsProp   -> s"${30.second.toMillis}"
-        )
-      )
-
-    ZIO.acquireRelease(
-      startKafka(embeddedKafkaConfig).map(EmbeddedKafkaService.apply)
-    )(_.stop())
-  }
+        ),
+      customBrokerProps
+    )
 
   /**
    * Will connect to a Kafka instance running on localhost:9092 (with Docker, for example).
@@ -104,6 +105,29 @@ object Kafka {
   val local: ULayer[Kafka] = ZLayer.succeed(DefaultLocal)
 
   final case class Sasl(value: Kafka) extends AnyVal
+
+  private def embeddedWithBrokerProps(
+    presetProps: Ports => Map[String, String],
+    customProps: Ports => Map[String, String]
+  ): ZLayer[Any, Throwable, Kafka] =
+    ZLayer.scoped {
+      for {
+        ports <- nextPorts
+        brokerProps = presetProps(ports) ++ customProps(ports) // custom is after to allow overriding
+        embeddedKafkaConfig = EmbeddedKafkaConfig(
+                                ports.kafkaPort,
+                                ports.zookeeperPort,
+                                brokerProps
+                              )
+        kafka <- ZIO.acquireRelease(
+                   ZIO
+                     .attemptBlocking(EmbeddedKafkaService(EmbeddedKafka.start()(embeddedKafkaConfig)))
+                     .catchNonFatalOrDie { e =>
+                       ZIO.fail(EmbeddedKafkaStartException("Failed to start embedded Kafka", e))
+                     }
+                 )(_.stop())
+      } yield kafka
+    }
 
   final case class EmbeddedKafkaService(embeddedK: EmbeddedK) extends Kafka {
     override def bootstrapServers: List[String] = List(s"localhost:${embeddedK.config.kafkaPort}")
@@ -115,17 +139,10 @@ object Kafka {
     override def stop(): UIO[Unit]              = ZIO.unit
   }
 
-  private val ports                 = new AtomicReference[(Int, Int)](6001 -> 7001)
-  private def nextPorts: (Int, Int) = ports.getAndUpdate { case (k, z) => (k + 1, z + 1) }
+  final case class Ports(kafkaPort: Int, zookeeperPort: Int)
 
-  private def startKafka(makeConfig: (Int, Int) => EmbeddedKafkaConfig): Task[EmbeddedK] =
-    ZIO.attemptBlocking {
-      try {
-        val (k, z) = nextPorts
-        EmbeddedKafka.start()(makeConfig(k, z))
-      } catch {
-        case NonFatal(e) => throw EmbeddedKafkaStartException("Failed to start embedded Kafka", e)
-      }
-    }
+  private val ref = Ref.unsafe.make(Ports(6001, 7001))(Unsafe.unsafe)
 
+  private val nextPorts: ZIO[Any, Nothing, Ports] =
+    ref.getAndUpdate(ports => Ports(ports.kafkaPort + 1, ports.zookeeperPort + 1))
 }
