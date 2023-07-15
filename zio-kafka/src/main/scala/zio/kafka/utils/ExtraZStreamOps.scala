940cac69a7b6fa42cae2dba35847e55cc034aa0e
jules Ivanic <jules.ivanic@gmail.com>
2023-07-15 17:55:32 +0400

    Try to implement the "one timer per thread"

--- zio-kafka/src/main/scala/zio/kafka/utils/ExtraZStreamOps.scala
+++ zio-kafka/src/main/scala/zio/kafka/utils/ExtraZStreamOps.scala
@@ -3,9 +3,38 @@ package zio.kafka.utils
 import zio._
 import zio.stream._
 
+import java.time.temporal.ChronoUnit
+
 object ExtraZStreamOps {
 
+  // noinspection SimplifyWhenInspection
   implicit class ZStreamOps[R, E, A](val stream: ZStream[R, E, A]) extends AnyVal {
+
+    type Interruptor[E1 >: E] = Promise[E1, Unit]
+    type ResetTimer           = UIO[Unit]
+
+    private def timer[E1 >: E](
+      e: => E1
+    )(after: Duration): URIO[Scope, (Interruptor[E1], ResetTimer)] = {
+      def getNow: UIO[Long] = Clock.currentTime(ChronoUnit.MILLIS)
+
+      for {
+        scope                  <- ZIO.environment[Scope]
+        now                    <- getNow
+        lastChunkReceivedAtRef <- Ref.make(now)
+        started                <- Ref.Synchronized.make(false)
+        p                      <- Promise.make[E1, Unit]
+        afterAsMillis = after.toMillis
+        tick =
+          (getNow zip lastChunkReceivedAtRef.get).flatMap { case (now, lastExecution) =>
+            val deadline = lastExecution + afterAsMillis
+            if (deadline < now) p.fail(e) else ZIO.unit
+          }
+            .repeat(Schedule.fixed(1.second))
+        startTimer = started.updateSomeZIO { case false => tick.forkScoped.provideEnvironment(scope).as(true) }
+        resetTimer = startTimer *> getNow.flatMap(lastChunkReceivedAtRef.set(_))
+      } yield (p, resetTimer)
+    }
 
     /**
      * Fails the stream with given error if it is not consumed (pulled) from, for some duration.
@@ -15,26 +44,13 @@ object ExtraZStreamOps {
     def consumeTimeoutFail[E1 >: E](e: => E1)(after: Duration): ZStream[R, E1, A] =
       // For every incoming chunk a timer is started. When the chunk is consumed, the timer is stopped by interrupting
       // it. When the timer completes, the stream gets interrupted.
-      stream.via(
-        ZPipeline.unwrapScoped(
-          for {
-            scope <- ZIO.environment[Scope]
-            p     <- Promise.make[E1, Unit]
-          } yield {
-            val timer = p.fail(e).delay(after).forkScoped.provideEnvironment(scope)
-            def loop: ZChannel[Any, ZNothing, Chunk[A], Any, E, Chunk[A], Unit] =
-              ZChannel.readWithCause(
-                (in: Chunk[A]) =>
-                  ZChannel.fromZIO(timer).flatMap { t =>
-                    ZChannel.write(in) *> ZChannel.fromZIO(t.interrupt) *> loop
-                  },
-                (cause: Cause[ZNothing]) => ZChannel.refailCause(cause),
-                (_: Any) => ZChannel.unit
-              )
-            ZPipeline.fromChannel(loop.interruptWhen(p))
-          }
-        )
-      )
+      ZStream.unwrapScoped {
+        for {
+          (p, resetTimer) <- timer(e)(after)
+        } yield stream
+          .viaFunction(_.interruptWhen(p))
+          .mapChunksZIO(data => resetTimer *> ZIO.succeed(data))
+      }
   }
 
 }
