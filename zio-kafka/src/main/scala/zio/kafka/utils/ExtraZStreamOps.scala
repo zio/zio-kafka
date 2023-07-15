c8b84b1c3441edfb5959ffebc4791b77bb9cafeb
jules Ivanic <jules.ivanic@gmail.com>
2023-07-15 18:33:34 +0400

    Unsimplfy code

--- zio-kafka/src/main/scala/zio/kafka/utils/ExtraZStreamOps.scala
+++ zio-kafka/src/main/scala/zio/kafka/utils/ExtraZStreamOps.scala
@@ -3,37 +3,40 @@ package zio.kafka.utils
 import zio._
 import zio.stream._
 
-import java.time.temporal.ChronoUnit
-
 object ExtraZStreamOps {
 
   // noinspection SimplifyWhenInspection
   implicit class ZStreamOps[R, E, A](val stream: ZStream[R, E, A]) extends AnyVal {
 
     type Interruptor[E1 >: E] = Promise[E1, Unit]
+    type StartTimer           = UIO[Unit]
     type ResetTimer           = UIO[Unit]
 
     private def timer[E1 >: E](
       e: => E1
-    )(after: Duration): UIO[(Interruptor[E1], ResetTimer)] = {
-      def getNow: UIO[Long] = Clock.currentTime(ChronoUnit.MILLIS)
-
+    )(after: Duration): URIO[Scope, (Interruptor[E1], StartTimer, ResetTimer)] =
       for {
-        now                    <- getNow
+        scope                  <- ZIO.scope
+        now                    <- Clock.instant
         lastChunkReceivedAtRef <- Ref.Synchronized.make(now)
         p                      <- Promise.make[E1, Unit]
         afterAsMillis = after.toMillis
-        failIfNeeded = (now: Long) =>
-                         lastChunkReceivedAtRef.updateZIO { lastChunkReceivedAt =>
-                           val deadline         = lastChunkReceivedAt + afterAsMillis
-                           val deadlineIsPassed = deadline < now
+        failPromiseIfNeeded = Clock.instant.flatMap { now =>
+                                lastChunkReceivedAtRef.updateZIO { lastChunkReceivedAt =>
+                                  val deadline         = lastChunkReceivedAt.plusMillis(afterAsMillis)
+                                  val deadlineIsPassed = deadline.isBefore(now)
 
-                           if (deadlineIsPassed) p.fail(e).as(lastChunkReceivedAt)
-                           else ZIO.succeed(now)
-                         }
-        resetTimer = getNow.flatMap(now => failIfNeeded(now)).unit
-      } yield (p, resetTimer)
-    }
+                                  if (deadlineIsPassed) p.fail(e).as(lastChunkReceivedAt)
+                                  else ZIO.succeed(now)
+                                }
+                              }
+        startBackgroundInterruptorIfNot <-
+          failPromiseIfNeeded
+            .repeat(Schedule.fixed(1.second))
+            .forkIn(scope)
+            .once
+        resetTimer = failPromiseIfNeeded
+      } yield (p, startBackgroundInterruptorIfNot, resetTimer)
 
     /**
      * Fails the stream with given error if it is not consumed (pulled) from, for some duration.
@@ -43,12 +46,12 @@ object ExtraZStreamOps {
     def consumeTimeoutFail[E1 >: E](e: => E1)(after: Duration): ZStream[R, E1, A] =
       // For every incoming chunk a timer is started. When the chunk is consumed, the timer is stopped by interrupting
       // it. When the timer completes, the stream gets interrupted.
-      ZStream.unwrap {
+      ZStream.unwrapScoped {
         for {
-          (p, resetTimer) <- timer(e)(after)
+          (p, startTimer, resetTimer) <- timer(e)(after)
         } yield stream
           .interruptWhen(p)
-          .mapChunksZIO(resetTimer.as(_))
+          .mapChunksZIO(data => startTimer *> resetTimer.as(data))
       }
   }
 
