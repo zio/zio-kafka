624e421e18eefa26d8f2ecdc55db40011b706d7c
Erik van Oosten <e.vanoosten@grons.nl>
2023-07-16 10:52:06 +0200

    Add extra tests as suggested by @adamgfraser

--- zio-kafka-test/src/test/scala/zio/kafka/utils/ExtraZStreamOpsSpec.scala
+++ zio-kafka-test/src/test/scala/zio/kafka/utils/ExtraZStreamOpsSpec.scala
@@ -13,12 +13,13 @@ object ExtraZStreamOpsSpec extends ZIOSpecDefaultSlf4j {
 
   private object ConsumeTimeout extends RuntimeException with NoStackTrace
 
+  private val stream10 = ZStream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).rechunk(1)
+
   override def spec: Spec[TestEnvironment, Any] =
     suite("ExtraZStreamOps")(
       test("consumeTimeoutFail does not fail stream for fast consumer") {
         for {
-          f <- ZStream
-                 .fromIterator(Iterator.from(1), 1)
+          f <- stream10
                  .consumeTimeoutFail(ConsumeTimeout)(3.seconds)
                  .tap(_ => ZIO.sleep(1.second))
                  .take(5)
@@ -43,11 +44,9 @@ object ExtraZStreamOpsSpec extends ZIOSpecDefaultSlf4j {
       test("consumeTimeoutFail fails stream for slow consumer") {
         for {
           consumedRef <- Ref.make(Seq.empty[Int])
-          f <- ZStream
-                 .fromIterator(Iterator.from(1), 1)
+          f <- stream10
                  .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
                  .tap(elem => ZIO.sleep(200.seconds).when(elem == 4))
-                 .take(10)
                  .tap(elem => consumedRef.update(_ :+ elem))
                  .runDrain
                  .exit
@@ -57,10 +56,41 @@ object ExtraZStreamOpsSpec extends ZIOSpecDefaultSlf4j {
           consumed <- consumedRef.get
         } yield assert(fResult)(fails(equalTo(ConsumeTimeout))) && assertTrue(consumed == Seq(1, 2, 3, 4))
       },
+      test("consumeTimeoutFail fails stream when not consumed") {
+        for {
+          consumedRef <- Ref.make(Seq.empty[Int])
+          f <- stream10
+                 .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
+                 .tap(_ => ZIO.sleep(200.seconds))
+                 .tap(elem => consumedRef.update(_ :+ elem))
+                 .runDrain
+                 .exit
+                 .fork
+          _        <- TestClock.adjust(100.seconds).repeatN(2)
+          fResult  <- f.join
+          consumed <- consumedRef.get
+        } yield assert(fResult)(fails(equalTo(ConsumeTimeout))) && assertTrue(consumed.isEmpty)
+      },
+      test("consumeTimeoutFail fails stream when consumer stops pulling") {
+        for {
+          consumedRef <- Ref.make(Seq.empty[Int])
+          f <- stream10
+                 .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
+                 .tap(_ => ZIO.sleep(200.seconds))
+                 .take(1)
+                 .tap(elem => consumedRef.update(_ :+ elem))
+                 .runDrain
+                 .exit
+                 .fork
+          _        <- TestClock.adjust(100.seconds).repeatN(2)
+          fResult  <- f.join
+          consumed <- consumedRef.get
+        } yield assert(fResult)(fails(equalTo(ConsumeTimeout))) && assertTrue(consumed.size == 1)
+      },
       test("consumeTimeoutFail retains chunking structure") {
         for {
-          result <- ZStream
-                      .fromChunks(Chunk(1, 2, 3, 4, 5), Chunk(1, 2, 3, 4, 5))
+          result <- ZStream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                      .rechunk(5)
                       .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
                       .chunks
                       .runHead
