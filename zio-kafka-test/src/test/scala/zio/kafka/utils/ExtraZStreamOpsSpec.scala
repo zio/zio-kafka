eef9e05af7830d7ede0698dafa3fca570e09f51f
Erik van Oosten <e.vanoosten@grons.nl>
2023-07-16 16:37:57 +0200

    More (commented out) tests

--- zio-kafka-test/src/test/scala/zio/kafka/utils/ExtraZStreamOpsSpec.scala
+++ zio-kafka-test/src/test/scala/zio/kafka/utils/ExtraZStreamOpsSpec.scala
@@ -76,6 +76,27 @@ object ExtraZStreamOpsSpec extends ZIOSpecDefaultSlf4j {
           result <- pullExit.join
         } yield assert(result)(fails(isSome(equalTo(ConsumeTimeout))))
       },
+// A stream that is not read from is just a description, it cannot fail.
+// Therefore, the following test doesn't make sense:
+//      test("consumeTimeoutFail fail stream when pull doesn't come") {
+//        for {
+//          endP <- Promise.make[Nothing, ConsumeTimeout.type]
+//          timeoutStream = stream10
+//            .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
+//            .tapError(e => endP.succeed(e))
+//          pull <- timeoutStream.toPull
+//          endF <- (for {
+//                        _ <- pull.delay(50.seconds)
+//                        _ <- pull.delay(50.seconds)
+//                        _ <- ZIO.sleep(100.seconds)
+//                        e <- endP.await
+//                      } yield e).fork
+//          _      <- TestClock.adjust(50.second).schedule(Schedule.recurs(4))
+//          result <- endF.join
+//        } yield assertTrue(result == ConsumeTimeout)
+//      },
+// Since the wrapped stream only starts when pulling starts, we never time out waiting for the first pull.
+// Therefore, the following test doesn't make sense.
 //      test("consumeTimeoutFail fails stream when first pull is slow") {
 //        for {
 //          pull <- stream10
