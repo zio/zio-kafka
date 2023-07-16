f4880aca6f49903a147b62b658eb5682f4cfdcbd
Erik van Oosten <e.vanoosten@grons.nl>
2023-07-16 16:06:03 +0200

    Much cleaner implementation based on toPull

--- zio-kafka-test/src/test/scala/zio/kafka/utils/ExtraZStreamOpsSpec.scala
+++ zio-kafka-test/src/test/scala/zio/kafka/utils/ExtraZStreamOpsSpec.scala
@@ -16,86 +16,77 @@ object ExtraZStreamOpsSpec extends ZIOSpecDefaultSlf4j {
 
   private val stream10 = ZStream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).rechunk(1)
 
-  override def spec: Spec[TestEnvironment, Any] =
+  override def spec: Spec[TestEnvironment with Scope, Any] =
     suite("ExtraZStreamOps")(
-      test("consumeTimeoutFail does not fail stream for fast consumer") {
+      test("consumeTimeoutFail does not fail stream for fast producer") {
         for {
-          f <- stream10
-                 .consumeTimeoutFail(ConsumeTimeout)(3.seconds)
-                 .tap(_ => ZIO.sleep(1.second))
-                 .take(5)
-                 .runCollect
-                 .fork
-          _        <- TestClock.adjust(1.second).repeatN(5)
-          consumed <- f.join
-        } yield assertTrue(consumed.size == 5)
+          consumed <- stream10
+            .consumeTimeoutFail(ConsumeTimeout)(1.second)
+            .take(5)
+            .runCollect
+        } yield assertTrue(consumed == Chunk(1, 2, 3, 4, 5))
       },
       test("consumeTimeoutFail does not fail stream for slow producer") {
         for {
           f <- ZStream
-                 .fromSchedule(Schedule.fixed(5.seconds))
-                 .consumeTimeoutFail(ConsumeTimeout)(1.second)
-                 .take(5)
-                 .runCollect
-                 .fork
-          _        <- TestClock.adjust(1.seconds).repeatN(25)
+            .fromSchedule(Schedule.fixed(5.seconds))
+            .consumeTimeoutFail(ConsumeTimeout)(1.second)
+            .take(5)
+            .runCollect
+            .fork
+          _ <- TestClock.adjust(1.seconds).repeatN(25)
           consumed <- f.join
-        } yield assertTrue(consumed.size == 5)
-      },
-      test("consumeTimeoutFail fails stream for slow consumer") {
-        for {
-          consumedRef <- Ref.make(Seq.empty[Int])
-          f <- stream10
-                 .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
-                 .tap(elem => ZIO.sleep(200.seconds).when(elem == 4))
-                 .tap(elem => consumedRef.update(_ :+ elem))
-                 .runDrain
-                 .exit
-                 .fork
-          _        <- TestClock.adjust(100.seconds).repeatN(2)
-          fResult  <- f.join
-          consumed <- consumedRef.get
-        } yield assert(fResult)(fails(equalTo(ConsumeTimeout))) && assertTrue(consumed == Seq(1, 2, 3, 4))
-      },
-      test("consumeTimeoutFail fails stream when not consumed") {
-        for {
-          consumedRef <- Ref.make(Seq.empty[Int])
-          f <- stream10
-                 .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
-                 .tap(_ => ZIO.sleep(200.seconds))
-                 .tap(elem => consumedRef.update(_ :+ elem))
-                 .runDrain
-                 .exit
-                 .fork
-          _        <- TestClock.adjust(100.seconds).repeatN(2)
-          fResult  <- f.join
-          consumed <- consumedRef.get
-        } yield assert(fResult)(fails(equalTo(ConsumeTimeout))) && assertTrue(consumed.isEmpty)
-      },
-      test("consumeTimeoutFail fails stream when consumer stops pulling") {
-        for {
-          consumedRef <- Ref.make(Seq.empty[Int])
-          f <- stream10
-                 .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
-                 .tap(_ => ZIO.sleep(200.seconds))
-                 .take(1)
-                 .tap(elem => consumedRef.update(_ :+ elem))
-                 .runDrain
-                 .exit
-                 .fork
-          _        <- TestClock.adjust(100.seconds).repeatN(2)
-          fResult  <- f.join
-          consumed <- consumedRef.get
-        } yield assert(fResult)(fails(equalTo(ConsumeTimeout))) && assertTrue(consumed.size == 1)
+        } yield assertTrue(consumed == Chunk[Long](0, 1, 2, 3, 4))
       },
       test("consumeTimeoutFail retains chunking structure") {
         for {
           result <- ZStream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-                      .rechunk(5)
-                      .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
-                      .chunks
-                      .runHead
+            .rechunk(5)
+            .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
+            .chunks
+            .runHead
         } yield assertTrue(result.map(_.size).getOrElse(0) == 5)
+      },
+      test("consumeTimeoutFail does not fail stream for fast consumer") {
+        for {
+          consumedRef <- Ref.make(Seq.empty[Int])
+          pull     <- stream10
+                     .consumeTimeoutFail(ConsumeTimeout)(3.seconds)
+                     .toPull
+          f        <- pull
+                     .tap(chunk => consumedRef.update(_ ++ chunk))
+                     .schedule(Schedule.spaced(1.second) && Schedule.recurs(5))
+                     .fork
+          _        <- TestClock.adjust(1.second).schedule(Schedule.recurs(5))
+          _        <- f.join
+          consumed <- consumedRef.get
+        } yield assertTrue(consumed.size == 5)
+      },
+      test("consumeTimeoutFail fail stream for slow consumer") {
+        for {
+          pull     <- stream10
+                     .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
+                     .toPull
+          pullExit <- (for {
+                         _ <- pull.delay(50.seconds)
+                         _ <- pull.delay(50.seconds)
+                         e <- pull.delay(200.second).exit
+                       } yield e
+                      )
+                       .fork
+          _        <- TestClock.adjust(50.second).schedule(Schedule.recurs(6))
+          result   <- pullExit.join
+        } yield assert(result)(fails(isSome(equalTo(ConsumeTimeout))))
+      },
+      test("consumeTimeoutFail fails stream when first pull is slow") {
+        for {
+          pull     <- stream10
+                     .consumeTimeoutFail(ConsumeTimeout)(100.seconds)
+                     .toPull
+          pullExit <- pull.delay(200.second).exit.fork
+          _        <- TestClock.adjust(50.second).schedule(Schedule.recurs(6))
+          result   <- pullExit.join
+        } yield assert(result)(fails(isSome(equalTo(ConsumeTimeout))))
       }
     ) @@ timeout(5.seconds)
 
--- zio-kafka/src/main/scala/zio/kafka/utils/ExtraZStreamOps.scala
+++ zio-kafka/src/main/scala/zio/kafka/utils/ExtraZStreamOps.scala
@@ -10,30 +10,27 @@ object ExtraZStreamOps {
     /**
      * Fails the stream with given error if it is not consumed (pulled) from, for some duration.
      *
+     * Note: since the wrapped stream only starts when pulling starts, we never time out waiting for the first pull
+     *
      * Also see [[zio.stream.ZStream.timeoutFail]] for failing the stream doesn't _produce_ a value.
      */
     def consumeTimeoutFail[E1 >: E](e: => E1)(after: Duration): ZStream[R, E1, A] =
-      // For every incoming chunk a timer is started. When the chunk is consumed, the timer is stopped by interrupting
-      // it. When the timer completes, the stream gets interrupted.
-      stream.via(
-        ZPipeline.unwrapScoped(
-          for {
-            scope <- ZIO.environment[Scope]
-            p     <- Promise.make[E1, Unit]
-          } yield {
-            val timer = p.fail(e).delay(after).forkScoped.provideEnvironment(scope)
-            def loop: ZChannel[Any, ZNothing, Chunk[A], Any, E, Chunk[A], Unit] =
-              ZChannel.readWithCause(
-                (in: Chunk[A]) =>
-                  ZChannel.fromZIO(timer).flatMap { t =>
-                    ZChannel.write(in) *> ZChannel.fromZIO(t.interrupt) *> loop
-                  },
-                (cause: Cause[ZNothing]) => ZChannel.refailCause(cause),
-                (_: Any) => ZChannel.unit
-              )
-            ZPipeline.fromChannel(loop.interruptWhen(p))
-          }
-        )
+      ZStream.unwrap(
+        for {
+          streamStart <- Clock.nanoTime
+          pullEndRef  <- Ref.make(streamStart)
+        } yield {
+          val pull: ZIO[Scope with R, Nothing, ZIO[R, Option[E1], Chunk[A]]] =
+            stream.toPull.map { pull =>
+              for {
+                pullEnd    <- pullEndRef.get
+                now        <- Clock.nanoTime
+                pullResult <- if ((now - pullEnd).nanos < after) pull else ZIO.fail(Some(e))
+                _          <- Clock.nanoTime.flatMap(now => pullEndRef.set(now).unit)
+              } yield pullResult
+            }
+          ZStream.fromPull[R, E1, A](pull)
+        }
       )
   }
 
