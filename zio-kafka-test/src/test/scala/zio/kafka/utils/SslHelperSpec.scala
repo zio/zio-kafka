c9e37d23377ae4897a2384a0f90c3f307efdd3e5
Jules Ivanic <guizmaii@users.noreply.github.com>
2023-07-18 18:45:44 +0400

    Fix: Make the `SslHelper.validateEndpoint` as resilient as Kafka. If some Kafka bootstraps servers are down, that shouldn't affect the result of the validation  (#950)
    Co-authored-by: Erik van Oosten <e.vanoosten@grons.nl>

--- zio-kafka-test/src/test/scala/zio/kafka/utils/SslHelperSpec.scala
+++ zio-kafka-test/src/test/scala/zio/kafka/utils/SslHelperSpec.scala
@@ -2,25 +2,31 @@ package zio.kafka.utils
 
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.KafkaException
-import zio.ZIO
+import org.apache.kafka.common.utils.Utils
 import zio.kafka.ZIOSpecDefaultSlf4j
+import zio.kafka.admin.AdminClientSettings
 import zio.kafka.consumer.{ Consumer, Subscription }
 import zio.kafka.producer.Producer
 import zio.kafka.serde.Serde
+import zio.kafka.testkit.KafkaTestUtils.adminSettings
 import zio.kafka.testkit.{ Kafka, KafkaRandom, KafkaTestUtils }
 import zio.test.Assertion._
 import zio.test.TestAspect._
 import zio.test._
+import zio.{ durationInt, Scope, Task, ZIO }
+
+import java.nio.channels.SocketChannel
 
 /**
  * This test checks the fix for the issue https://issues.apache.org/jira/browse/KAFKA-4090
  */
+//noinspection SimplifyAssertInspection
 object SslHelperSpec extends ZIOSpecDefaultSlf4j with KafkaRandom {
 
   override val kafkaPrefix: String = "oom-spec"
 
-  override def spec: Spec[TestEnvironment, Any] =
-    suite(".validateEndpoint")(
+  private val integrationTests =
+    suite("Integration tests")(
       test("Producer should fail due to ssl check") {
         for {
           result <- (
@@ -107,5 +113,195 @@ object SslHelperSpec extends ZIOSpecDefaultSlf4j with KafkaRandom {
           )
         )
       }
-    ).provideLayerShared(Kafka.sslEmbedded) @@ withLiveClock @@ sequential
+    ).provideLayerShared(Kafka.sslEmbedded)
+
+  private val unitTests =
+    suite("Unit tests")(
+      test("fails if the passed list of servers is empty") {
+        val result = SslHelper.validateEndpoint(List.empty, Map.empty)
+
+        assertZIO(result.exit)(
+          fails(
+            isSubtype[KafkaException](hasMessage(equalTo("Failed to create new KafkaAdminClient"))) &&
+              hasThrowableCause(
+                isSubtype[IllegalArgumentException](
+                  hasMessage(equalTo("Empty bootstrapServers list"))
+                )
+              )
+          )
+        )
+      },
+      suite("is resilient: if some Nodes, not all, are down that doesn't affect the result of the function")(
+        test("succeeds if some of the nodes are down and the SSL configuration is correct") {
+          val result: Task[Unit] =
+            (
+              for {
+                settings <- adminSettings // Non-SSL settings used with a non-SSL cluster
+                server0              = settings.bootstrapServers.head
+                address0             = Utils.getHost(server0)
+                port0                = Utils.getPort(server0)
+                server1              = s"$address0:${port0 + 6000}"
+                settingsWithDownNode = settings.copy(bootstrapServers = settings.bootstrapServers :+ server1)
+                // We simulate that the Socket opening fails for some Nodes but not all
+                result <- SslHelper.doValidateEndpoint { address =>
+                            val isValidAddress = address.getPort == port0
+
+                            if (isValidAddress) SocketChannel.open(address)
+                            else throw new java.net.ConnectException("Connection refused")
+                          }(settingsWithDownNode.bootstrapServers, settingsWithDownNode.properties)
+              } yield result
+            ).provide(Kafka.embedded)
+
+          assertZIO(result.exit)(succeeds(anything))
+        },
+        test("fails if some of the nodes are down and the SSL configuration is incorrect") {
+          val result: Task[Unit] =
+            (
+              for {
+                settings <- adminSettings // Non-SSL settings used with an SSL-configured cluster
+                server0              = settings.bootstrapServers.head
+                address0             = Utils.getHost(server0)
+                port0                = Utils.getPort(server0)
+                server1              = s"$address0:${port0 + 6000}"
+                settingsWithDownNode = settings.copy(bootstrapServers = settings.bootstrapServers :+ server1)
+                // We simulate that the Socket opening fails for some Nodes but not all
+                result <- SslHelper.doValidateEndpoint { address =>
+                            val isValidAddress = address.getPort == port0
+
+                            if (isValidAddress) SocketChannel.open(address)
+                            else throw new java.net.ConnectException("Connection refused")
+                          }(settingsWithDownNode.bootstrapServers, settingsWithDownNode.properties)
+              } yield result
+            ).provide(Kafka.sslEmbedded)
+
+          assertZIO(result.exit)(
+            fails(
+              isSubtype[KafkaException](hasMessage(equalTo("Failed to create new KafkaAdminClient"))) &&
+                hasThrowableCause(
+                  isSubtype[IllegalArgumentException](
+                    hasMessage(
+                      equalTo(
+                        "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+                      )
+                    )
+                  )
+                )
+            )
+          )
+        },
+        test("fails if all the nodes are down") {
+          val result: Task[Unit] =
+            for {
+              _ <- ZIO.unit
+              // We don't care about the bootstraps servers here as we'll simulate that all Nodes are down
+              settings             = AdminClientSettings(List("localhost:9999"))
+              server0              = settings.bootstrapServers.head
+              address0             = Utils.getHost(server0)
+              port0                = Utils.getPort(server0)
+              server1              = s"$address0:${port0 + 6000}"
+              settingsWithDownNode = settings.copy(bootstrapServers = settings.bootstrapServers :+ server1)
+              // We simulate that the Socket opening always fails (ie. that all Nodes are down)
+              result <-
+                SslHelper.doValidateEndpoint(_ => throw new java.net.ConnectException("Connection refused. 💥!"))(
+                  settingsWithDownNode.bootstrapServers,
+                  settingsWithDownNode.properties
+                )
+            } yield result
+
+          assertZIO(result.exit)(
+            fails(
+              isSubtype[KafkaException](hasMessage(equalTo("Failed to create new KafkaAdminClient"))) &&
+                hasThrowableCause(isSubtype[java.net.ConnectException](hasMessage(equalTo("Connection refused. 💥!"))))
+            )
+          )
+        },
+        suite("timeout mechanism")(
+          test("The `SocketChannel` is closed when we timeout the `SocketChannel.open` call") {
+            // Using a private IP address allows us to simulate a Node that is not reachable
+            // See:
+            //   - https://stackoverflow.com/questions/44929981/how-to-simulate-network-delay-on-a-local-port#comment76837344_44929981
+            //   - https://stackoverflow.com/a/904609/2431728
+            val host = "10.255.255.1"
+            val port = "9999"
+            val settings = AdminClientSettings(List(s"$host:$port"))
+              .withProperty("request.timeout.ms", 1.second.toMillis.toString)
+
+            val array = Array.fill[Any](3)(null)
+
+            val result =
+              SslHelper.doValidateEndpoint { address =>
+                array(0) = s"Starting creating SocketChannel for address: $address"
+                try {
+                  val socket = SocketChannel.open(address) // blocking call
+                  // This will never be called as the ZIO will be interrupted before
+                  array(1) = s"Done creating SocketChannel for address: $address"
+                  socket
+                } catch {
+                  case e: Throwable =>
+                    array(2) = e
+                    throw e
+                }
+              }(settings.bootstrapServers, settings.properties)
+
+            // custom assertion. More readable with a proper name.
+            val hasNoMessage = not(hasMessage(anything))
+
+            assertZIO(result.exit)(
+              fails(
+                isSubtype[KafkaException](hasMessage(equalTo("Failed to create new KafkaAdminClient"))) &&
+                  hasThrowableCause(
+                    isSubtype[java.util.concurrent.TimeoutException](
+                      hasMessage(equalTo(s"Failed to contact /10.255.255.1:$port"))
+                    )
+                  )
+              )
+            ) &&
+            assert(array(0))(equalTo("Starting creating SocketChannel for address: /10.255.255.1:9999")) &&
+            assert(array(1))(isNull) &&
+            assert(array(2))(isSubtype[java.nio.channels.ClosedByInterruptException](hasNoMessage))
+          } @@ nonFlaky(5),
+          test("The `SocketChannel` is closed when an exterior interruption is triggered") {
+            // Using a private IP address allows us to simulate a Node that is not reachable
+            // See:
+            //   - https://stackoverflow.com/questions/44929981/how-to-simulate-network-delay-on-a-local-port#comment76837344_44929981
+            //   - https://stackoverflow.com/a/904609/2431728
+            val host = "10.255.255.1"
+            val port = "9999"
+            val settings = AdminClientSettings(List(s"$host:$port"))
+              .withProperty("request.timeout.ms", 30.second.toMillis.toString)
+
+            val array = Array.fill[Any](3)(null)
+
+            val result =
+              SslHelper.doValidateEndpoint { address =>
+                array(0) = s"Starting creating SocketChannel for address: $address"
+                try {
+                  val socket = SocketChannel.open(address) // blocking call
+                  // This will never be called as the ZIO will be interrupted before
+                  array(1) = s"Done creating SocketChannel for address: $address"
+                  socket
+                } catch {
+                  case e: Throwable =>
+                    array(2) = e
+                    throw e
+                }
+              }(settings.bootstrapServers, settings.properties).fork.flatMap(fiber => fiber.interrupt.delay(1.seconds))
+
+            // custom assertion. More readable with a proper name.
+            val hasNoMessage = not(hasMessage(anything))
+
+            assertZIO(result)(isInterrupted) &&
+            assert(array(0))(equalTo("Starting creating SocketChannel for address: /10.255.255.1:9999")) &&
+            assert(array(1))(isNull) &&
+            assert(array(2))(isSubtype[java.nio.channels.ClosedByInterruptException](hasNoMessage))
+          } @@ nonFlaky(5)
+        )
+      )
+    )
+
+  override def spec: Spec[TestEnvironment with Scope, Any] =
+    suite(".validateEndpoint")(
+      integrationTests,
+      unitTests
+    ) @@ withLiveClock @@ sequential
 }
--- zio-kafka/src/main/scala/zio/kafka/utils/SslHelper.scala
+++ zio-kafka/src/main/scala/zio/kafka/utils/SslHelper.scala
@@ -5,11 +5,13 @@ import org.apache.kafka.common.KafkaException
 import org.apache.kafka.common.network.TransferableChannel
 import org.apache.kafka.common.protocol.ApiKeys
 import org.apache.kafka.common.requests.{ ApiVersionsRequest, RequestHeader }
-import zio.{ Task, ZIO }
+import zio.{ durationInt, durationLong, BuildFrom, Duration, IO, Task, Trace, URIO, ZIO }
 
+import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.{ FileChannel, SocketChannel }
 import scala.jdk.CollectionConverters._
+import scala.util.control.{ NoStackTrace, NonFatal }
 
 /**
  * This function validates that your Kafka client (Admin, Consumer, or Producer) configurations are valid for the Kafka
@@ -21,61 +23,180 @@ import scala.jdk.CollectionConverters._
  * Credits for this work go to Nick Pavlov (https://github.com/gurinderu), Guillaume Bécan (https://github.com/gbecan)
  * and the Conduktor (https://www.conduktor.io/) devs team.
  */
+//noinspection SimplifyUnlessInspection,SimplifyWhenInspection
 object SslHelper {
-  def validateEndpoint(bootstrapServers: List[String], props: Map[String, AnyRef]): Task[Unit] =
-    ZIO
-      .unless(
-        props
-          .get("security.protocol")
-          .exists {
-            case x: String if x.toUpperCase().contains("SSL") => true
-            case _                                            => false
+
+  /**
+   * A private exception that we use to "tag" some exceptions that we potentially want to ignore.
+   */
+  private final case class ConnectionError(cause: Throwable) extends NoStackTrace
+
+  // ⚠️ Must not do anything else than calling `doValidateEndpoint`. The algorithm of this function must be completely contained in `doValidateEndpoint`.
+  def validateEndpoint(bootstrapServers: List[String], props: Map[String, AnyRef]): IO[KafkaException, Unit] =
+    doValidateEndpoint(SocketChannel.open)(bootstrapServers, props)
+
+  /**
+   * We use this private function so that we can easily manipulate the `openSocket` function in unit-tests.
+   */
+  private[utils] def doValidateEndpoint(
+    unsafeOpenSocket: InetSocketAddress => SocketChannel // Handy for unit-tests
+  )(bootstrapServers: List[String], props: Map[String, AnyRef]): IO[KafkaException, Unit] = {
+    @inline def `request.timeout.ms`: Duration = {
+      val defaultValue = 30.seconds
+
+      props.get("request.timeout.ms") match {
+        case None => defaultValue
+        case Some(raw) =>
+          try {
+            val v = raw.toString.toLong
+            if (v <= 0) defaultValue else v.millis
+          } catch {
+            case NonFatal(_) => defaultValue
           }
-      ) {
-        ZIO.blocking {
-          for {
-            address <- ZIO.attempt {
-                         ClientUtils
-                           .parseAndValidateAddresses(bootstrapServers.asJava, ClientDnsLookup.USE_ALL_DNS_IPS)
-                           .asScala
-                           .toList
-                       }
-            _ <- ZIO.foreachParDiscard(address) { addr =>
-                   ZIO.scoped {
-                     for {
-                       channel <- ZIO.acquireRelease(
-                                    ZIO.attempt(SocketChannel.open(addr))
-                                  )(channel => ZIO.attempt(channel.close()).orDie)
-                       tls <- ZIO.attempt {
-                                // Send a simple request to check if the cluster accepts the connection
-                                sendTestRequest(channel)
-                                val buffer = readAnswerFromTestRequest(channel)
-                                isTls(buffer)
-                              }
-                       _ <-
-                         ZIO.when(tls) {
-                           ZIO.fail(
-                             new IllegalArgumentException(
-                               s"Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
-                             )
-                           )
-                         }
-                     } yield ()
+      }
+    }
+
+    if (bootstrapServers.isEmpty) ZIO.fail(kafkaException(new IllegalArgumentException("Empty bootstrapServers list")))
+    else
+      ZIO
+        .unless(
+          props
+            .get("security.protocol")
+            .exists {
+              case x: String if x.toUpperCase().contains("SSL") => true
+              case _                                            => false
+            }
+        ) {
+          ZIO.blocking {
+            for {
+              addresses <- ZIO.attempt {
+                             ClientUtils
+                               .parseAndValidateAddresses(bootstrapServers.asJava, ClientDnsLookup.USE_ALL_DNS_IPS)
+                               .asScala
+                               .toList
+                           }
+              errors <- ZIO.collectAllFailuresPar(
+                          addresses.map(validateSslConfigOf(unsafeOpenSocket, socketTimeout = `request.timeout.ms`))
+                        )
+              atLeastOneBootstrapServerIsUp = errors.size < addresses.size
+              _ <- errors.partition(_.isInstanceOf[ConnectionError]) match {
+                     // If we have at least one "real error" (not our internal "ConnectionError"), we fail with the first one
+                     case (_, head :: _) => ZIO.fail(head)
+                     // If we have no errors or if we have some of our internal "ConnectionError" but at least one bootstrap server is up, we succeed
+                     case (Nil, Nil)                         => ZIO.unit
+                     case _ if atLeastOneBootstrapServerIsUp => ZIO.unit
+                     // If we have only "connection errors" and no bootstrap server is up, we fail with the first one
+                     // Note that we don't propagate our internal `ConnectionError` wrapper
+                     case (head :: _, _) => ZIO.fail(head.asInstanceOf[ConnectionError].cause)
                    }
-                 }
-          } yield ()
+            } yield ()
+          }
         }
-      }
-      .unit
-      .mapError { e =>
-        // Mimic behaviour of KafkaAdminClient.createInternal
-        new KafkaException("Failed to create new KafkaAdminClient", e)
-      }
+        .unit
+        .mapError(kafkaException)
+  }
+
+  /**
+   * Mimic behaviour of KafkaAdminClient.createInternal
+   */
+  private def kafkaException(e: Throwable): KafkaException =
+    new KafkaException("Failed to create new KafkaAdminClient", e)
+
+  /**
+   * Let's take some time here to discuss the algorithm of this function as it's a bit tricky and uses obscure Java
+   * APIs/features.
+   *
+   * The goal of this function is to validate that the SSL configuration of the client is correct for the Kafka cluster
+   * we want to contact. (ie. that the Kafka cluster is not configured with SSL as we previously validated that the
+   * client was not configured for an SSL server)
+   *
+   * The algorithm is the following:
+   *   1. We open a socket to the Kafka node
+   *   1. We send a simple request to the Kafka node to check if it's configured with SSL
+   *   1. We read the answer from the Kafka node
+   *   1. If the node is configured with SSL, we fail with an error
+   *
+   * This algorithm and its implementation, per se, are relatively simple. It's the few lines of code inside the
+   * `ZIO.attemptBlockingInterrupt` call.
+   *
+   * The tricky part is that we want to be able to timeout the whole process if it takes too long but timeouting a
+   * `SocketChannel` is, theoretically, not possible.
+   *
+   * Also, we don't want to leak memory so we want to be sure that the socket is closed in all cases (success, failure,
+   * or timeout/interruption).
+   *
+   * The socket might be closed in two possible cases:
+   *   1. The socket is successfully opened and we successfully sent the request to the Kafka cluster. In this case, the
+   *      `finally` block will close the socket.
+   *   1. The networking exchange takes too long and we timeout/interrupt the whole process. In this case, and that's
+   *      the most tricky/weird part, to close the socket, we interrupt the thread running it.
+   *
+   * Why does interrupting the thread running the networking exchange closes the socket? Because the `SocketChannel`
+   * class implements the `InterruptibleChannel` interface, and that's a property of this interface.
+   *
+   * From the documentation of `InterruptibleChannel`, we can read:
+   * {{{
+   * > A channel that implements this interface is also interruptible:
+   * > If a thread is blocked in an I/O operation on an interruptible channel then another thread may invoke the blocked thread's interrupt method.
+   * > This will cause the channel to be closed, the blocked thread to receive a ClosedByInterruptException, and the blocked thread's interrupt status to be set.
+   * }}}
+   *
+   * See: https://docs.oracle.com/javase/8/docs/api/java/nio/channels/InterruptibleChannel.html
+   *
+   * Let's recap.
+   *
+   * We use a `SocketChannel` to test the SSL configuration of the Kafka nodes. This `SocketChannel` is a resource and
+   * needs to be closed to avoid leaking memory. We want to be able to timeout the whole process if it takes too long
+   * but a `SocketChannel` is not timeoutable. So we implement a timeout mechanism that will interrupt the thread on
+   * which the `SocketChannel` is running to close it as it's one of the properties of the `SocketChannel` class.
+   *
+   * Note that there are unit-tests proving that this works.
+   *
+   * Useful links which helped to make this work:
+   *   - Discord discussion/thread with Vladimir Klyushnikov (https://github.com/vladimirkl) starting here:
+   *     https://discord.com/channels/629491597070827530/1122924033827164282/1124719212246610023
+   *   - https://stackoverflow.com/questions/2866557/timeout-for-socketchannel-doesnt-work#comment11463818_2866557
+   *   - https://stackoverflow.com/a/18375293/2431728
+   */
+  private def validateSslConfigOf(
+    unsafeOpenSocket: InetSocketAddress => SocketChannel,
+    socketTimeout: Duration
+  )(address: InetSocketAddress): Task[Unit] = {
+    @inline def unexpectedSslPacketError: IO[IllegalArgumentException, Nothing] =
+      ZIO.fail(
+        new IllegalArgumentException(
+          "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+        )
+      )
+
+    @inline def timeoutException: ConnectionError =
+      ConnectionError(new java.util.concurrent.TimeoutException(s"Failed to contact $address"))
+
+    ZIO.attemptBlockingInterrupt {
+      // Note about this algorithm:
+      // We make all the networking exchanges (ie. `unsafeOpenSocket`, `unsafeSendTestRequest` and `unsafeReadAnswerFromTestRequest`) in this
+      // interruptible blocking section so that we can easily timeout/interrupt the whole process if it takes too long.
+
+      val channel: SocketChannel =
+        try unsafeOpenSocket(address)
+        catch {
+          case NonFatal(e) => throw ConnectionError(e)
+        }
+
+      try {
+        unsafeSendTestRequest(channel)
+        val buffer = unsafeReadAnswerFromTestRequest(channel)
+        isTls(buffer)
+      } finally channel.close()
+    }
+      .timeoutFail(timeoutException)(socketTimeout)
+      .flatMap(isTLS => if (isTLS) unexpectedSslPacketError else ZIO.unit)
+  }
 
   /**
    * Send a simple request to check if connection can be established with current configuration
    */
-  private def sendTestRequest(channel: SocketChannel): Unit = {
+  private def unsafeSendTestRequest(channel: SocketChannel): Unit = {
     val transferableChannel = new TransferableChannel {
       override def hasPendingWrites: Boolean = false
 
@@ -104,7 +225,7 @@ object SslHelper {
   /**
    * Reads the 5 first bytes of the channel to extract the record type of the answer
    */
-  private def readAnswerFromTestRequest(channel: SocketChannel): ByteBuffer = {
+  private def unsafeReadAnswerFromTestRequest(channel: SocketChannel): ByteBuffer = {
     val buf = ByteBuffer.allocate(5)
     channel.read(buf)
     buf.position(0)
@@ -121,5 +242,16 @@ object SslHelper {
         true
       case _ => tlsMessageType >= 128
     }
+  }
+
+  private implicit final class ZIOTypeOps(private val dummy: ZIO.type) extends AnyVal {
+
+    /**
+     * Adapted from [[ZIO.collectAllSuccessesPar]]
+     */
+    def collectAllFailuresPar[R, E, A, Collection[+Element] <: Iterable[Element]](
+      in: Collection[ZIO[R, E, A]]
+    )(implicit bf: BuildFrom[Collection[ZIO[R, E, A]], E, Collection[E]], trace: Trace): URIO[R, Collection[E]] =
+      ZIO.collectAllWithPar(in.map(_.either)) { case Left(a) => a }.map(bf.fromSpecific(in))
   }
 }
