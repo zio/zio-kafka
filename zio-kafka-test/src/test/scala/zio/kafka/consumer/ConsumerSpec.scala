6ec678bf42769762a5ea3cc2d8c8001aef30ef57
Erik van Oosten <e.vanoosten@grons.nl>
2023-10-14 15:26:13 +0200

    Attempt to fix flaky test (2)
    Also: switch to nanoTime to prevent clock shifts

--- zio-kafka-test/src/test/scala/zio/kafka/consumer/ConsumerSpec.scala
+++ zio-kafka-test/src/test/scala/zio/kafka/consumer/ConsumerSpec.scala
@@ -337,13 +337,15 @@ object ConsumerSpec extends ZIOSpecDefaultSlf4j with KafkaRandom {
                           `max.poll.records` = 2
                         )
             consumer <- Consumer.make(settings.withPollTimeout(100.millis))
-            _        <- scheduledProduce(topic1, Schedule.fixed(100.millis).jittered).runDrain.forkScoped
-            _        <- scheduledProduce(topic2, Schedule.fixed(100.millis).jittered).runDrain.forkScoped
+            _        <- scheduledProduce(topic1, Schedule.fixed(50.millis).jittered).runDrain.forkScoped
+            _        <- scheduledProduce(topic2, Schedule.fixed(200.millis).jittered).runDrain.forkScoped
             // The slow consumer:
             c1 <- consumer
                     .plainStream(Subscription.topics(topic1), Serde.string, Serde.string)
                     .tap(r => ZIO.sleep(5.seconds).when(r.key == "key3"))
-                    .take(100) // Because of chunking, we need to pull a bit more before the interrupt kicks in.
+                    // Use `take` to ensure the test ends quickly, even when the interrupt fails to occur.
+                    // Because of chunking, we need to pull more than 3 records before the interrupt kicks in.
+                    .take(100)
                     .runDrain
                     .exit
                     .fork
