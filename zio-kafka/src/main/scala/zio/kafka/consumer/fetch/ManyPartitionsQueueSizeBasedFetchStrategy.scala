a5a157fa460f39995cbfb63eab26f69aead6daef
Erik van Oosten <e.vanoosten@grons.nl>
2024-07-15 21:40:00 +0200

    Completed scaladoc

--- zio-kafka/src/main/scala/zio/kafka/consumer/fetch/ManyPartitionsQueueSizeBasedFetchStrategy.scala
+++ zio-kafka/src/main/scala/zio/kafka/consumer/fetch/ManyPartitionsQueueSizeBasedFetchStrategy.scala
@@ -16,15 +16,21 @@ import scala.collection.mutable
  *   Maximum number of records to be buffered per partition. This buffer improves throughput and supports varying
  *   downstream message processing time, while maintaining some backpressure. Low values effectively disable prefetching
  *   in favour of low memory consumption. Large values leave it up to `maxTotalQueueSize` parameter to backpressure only
- *   on total memory usage.
+ *   over the buffers of all partitions together.
  *
  * The number of records that are fetched on every poll is controlled by the `max.poll.records` setting, the number of
  * records fetched for every partition is somewhere between 0 and `max.poll.records`.
  *
  * The default value for this parameter is 2 * the default `max.poll.records` of 500, rounded to the nearest power of 2.
+ *
  * @param maxTotalQueueSize
  *   Maximum number of records to be buffered over all partitions together. This can be used to limit memory usage when
  *   consuming a large number of partitions.
+ *
+ * When multiple streams are eligible for pre-fetching (because their queue size is below `maxPartitionQueueSize`), but
+ * together they exceed `maxTotalQueueSize`, then every call a random set of eligible streams is selected that stays
+ * below `maxTotalQueueSize`. The randomization ensures fairness and prevents read-starvation for streams at the end of
+ * the list.
  *
  * The default value is 20 * the default for `maxTotalQueueSize`, allowing approximately 20 partitions to do
  * pre-fetching in each poll.
@@ -37,7 +43,6 @@ final case class ManyPartitionsQueueSizeBasedFetchStrategy(
     streams: Chunk[PartitionStream]
   ): ZIO[Any, Nothing, Set[TopicPartition]] =
     for {
-      // By shuffling the streams we prevent read-starvation for streams at the end of the list.
       random          <- ZIO.random
       shuffledStreams <- random.shuffle(streams)
       tps <- ZIO
