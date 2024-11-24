b17024376e35a4b9bed51304ab61f3f5cf6e7473
Steven Vroonland <svroonland@gmail.com>
2024-11-24 20:25:57 +0100

    Styling

--- zio-kafka/src/main/scala/zio/kafka/consumer/internal/RebalanceCoordinator.scala
+++ zio-kafka/src/main/scala/zio/kafka/consumer/internal/RebalanceCoordinator.scala
@@ -164,12 +164,12 @@ private[internal] class RebalanceCoordinator(
           // Even if there is nothing to commit, continue to drive communication with the broker
           // so that commits can complete and the streams can make progress, by setting
           // executeOnEmpty = true
-          .tap(_ =>
+          .tap { _ =>
             committer.processQueuedCommits(
               (offsets, callback) => ZIO.attempt(consumer.commitAsync(offsets, callback)),
               executeOnEmpty = true
             )
-          )
+          }
           .takeWhile(_ => java.lang.System.nanoTime() <= deadline)
           .mapZIO(_ => endingStreamsCompletedAndCommitsExist)
           .takeUntil(completed => completed)
