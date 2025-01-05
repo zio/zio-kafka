5080c51863ecd1d498ea20ee63ece2c5779410b6
Erik van Oosten <e.vanoosten@grons.nl>
2025-01-05 12:59:09 +0100

    Document transactions
    Document the transactional producer introduced in #1434.

---
+++ zio-kafka-example/src/main/scala/zio/kafka/example/MyKafka.scala
@@ -0,0 +1,28 @@
+package zio.kafka.example
+
+import io.github.embeddedkafka.{ EmbeddedK, EmbeddedKafka, EmbeddedKafkaConfig }
+import zio._
+
+trait MyKafka {
+  def bootstrapServers: List[String]
+  def stop(): UIO[Unit]
+}
+
+object MyKafka {
+  final case class EmbeddedKafkaService(embeddedK: EmbeddedK) extends MyKafka {
+    override def bootstrapServers: List[String] = List(s"localhost:${embeddedK.config.kafkaPort}")
+    override def stop(): UIO[Unit]              = ZIO.succeed(embeddedK.stop(true))
+  }
+
+  val embedded: ZLayer[Any, Throwable, MyKafka] = ZLayer.scoped {
+    implicit val embeddedKafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig(
+      customBrokerProperties = Map(
+        "group.min.session.timeout.ms"     -> "500",
+        "group.initial.rebalance.delay.ms" -> "0",
+        "authorizer.class.name"            -> "kafka.security.authorizer.AclAuthorizer",
+        "super.users"                      -> "User:ANONYMOUS"
+      )
+    )
+    ZIO.acquireRelease(ZIO.attempt(EmbeddedKafkaService(EmbeddedKafka.start())))(_.stop())
+  }
+}
---
+++ zio-kafka-example/src/main/scala/zio/kafka/example/Transactional.scala
@@ -0,0 +1,92 @@
+package zio.kafka.example
+
+import org.apache.kafka.clients.producer.ProducerRecord
+import zio._
+import zio.kafka.consumer.diagnostics.Diagnostics
+import zio.kafka.consumer._
+import zio.kafka.producer._
+import zio.kafka.serde.Serde
+import zio.logging.backend.SLF4J
+
+import java.util.UUID
+
+object Transactional extends ZIOAppDefault {
+
+  /**
+   * See `zio-logging` documentation: https://zio.github.io/zio-logging/docs/overview/overview_slf4j
+   */
+  override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
+    zio.Runtime.removeDefaultLoggers >>> SLF4J.slf4j
+
+  private val topic = "transactional-test-topic"
+
+  private def consumerSettings: ZLayer[MyKafka, Throwable, ConsumerSettings] = ZLayer {
+    for {
+      kafka <- ZIO.service[MyKafka]
+    } yield ConsumerSettings(kafka.bootstrapServers)
+      .withGroupId("transactional-example-app")
+      .withRebalanceSafeCommits(true) // required for transactional producing
+      .withMaxRebalanceDuration(30.seconds)
+  }
+
+  private val producerSettings: ZLayer[MyKafka, Throwable, TransactionalProducerSettings] = ZLayer {
+    for {
+      kafka <- ZIO.service[MyKafka]
+    } yield TransactionalProducerSettings(
+      bootstrapServers = kafka.bootstrapServers,
+      transactionalId = UUID.randomUUID().toString
+    )
+  }
+
+  private val runConsumerStream: ZIO[Consumer & TransactionalProducer, Throwable, Unit] =
+    for {
+      consumer              <- ZIO.service[Consumer]
+      transactionalProducer <- ZIO.service[TransactionalProducer]
+      _                     <- ZIO.logInfo(s"Consuming messages from topic $topic...")
+      _ <- consumer
+             .plainStream(Subscription.topics(topic), Serde.int, Serde.long)
+             .mapChunks { records: Chunk[CommittableRecord[Int, Long]] =>
+               records.map { record =>
+                 val key: Int         = record.record.key()
+                 val value: Long      = record.record.value()
+                 val newValue: String = "Copy of " + value.toString
+
+                 val producerRecord: ProducerRecord[Int, String] =
+                   new ProducerRecord("my-output-topic", key, newValue)
+                 (producerRecord, record.offset)
+               }
+             }
+             .mapChunksZIO { recordsAndOffsets: Chunk[(ProducerRecord[Int, String], Offset)] =>
+               ZIO.scoped {
+                 for {
+                   _  <- ZIO.addFinalizer(ZIO.logInfo("Completing transaction"))
+                   tx <- transactionalProducer.createTransaction
+                   _ <- {
+                     val (records, offsets) = recordsAndOffsets.unzip
+                     tx.produceChunkBatch(
+                       records,
+                       Serde.int,
+                       Serde.string,
+                       OffsetBatch(offsets)
+                     )
+                   }
+                 } yield Chunk.empty
+               }.uninterruptible
+             }
+             .runDrain
+    } yield ()
+
+  override def run: ZIO[Scope, Any, Any] =
+    ZIO.logInfo("Starting app") *>
+      ZIO.addFinalizer(ZIO.logInfo("Stopping app")) *>
+      runConsumerStream
+        .provide(
+          consumerSettings,
+          ZLayer.succeed(Diagnostics.NoOp),
+          Consumer.live,
+          producerSettings,
+          TransactionalProducer.live,
+          MyKafka.embedded
+        )
+
+}
