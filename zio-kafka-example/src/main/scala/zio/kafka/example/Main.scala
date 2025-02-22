3e8656daa132126417337c56fcf2050d8d3402ce
Erik van Oosten <e.vanoosten@grons.nl>
2025-02-22 16:37:20 +0100

    Document transactions (#1432)
    Document the transactional producer introduced in #1434.

--- zio-kafka-example/src/main/scala/zio/kafka/example/Main.scala
+++ zio-kafka-example/src/main/scala/zio/kafka/example/Main.scala
@@ -1,35 +1,10 @@
 package zio.kafka.example
 
-import io.github.embeddedkafka.{ EmbeddedK, EmbeddedKafka, EmbeddedKafkaConfig }
 import zio._
 import zio.kafka.consumer.diagnostics.Diagnostics
 import zio.kafka.consumer.{ Consumer, ConsumerSettings, Subscription }
 import zio.kafka.serde.Serde
 import zio.logging.backend.SLF4J
-
-trait MyKafka {
-  def bootstrapServers: List[String]
-  def stop(): UIO[Unit]
-}
-
-object MyKafka {
-  final case class EmbeddedKafkaService(embeddedK: EmbeddedK) extends MyKafka {
-    override def bootstrapServers: List[String] = List(s"localhost:${embeddedK.config.kafkaPort}")
-    override def stop(): UIO[Unit]              = ZIO.succeed(embeddedK.stop(true))
-  }
-
-  val embedded: ZLayer[Any, Throwable, MyKafka] = ZLayer.scoped {
-    implicit val embeddedKafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig(
-      customBrokerProperties = Map(
-        "group.min.session.timeout.ms"     -> "500",
-        "group.initial.rebalance.delay.ms" -> "0",
-        "authorizer.class.name"            -> "kafka.security.authorizer.AclAuthorizer",
-        "super.users"                      -> "User:ANONYMOUS"
-      )
-    )
-    ZIO.acquireRelease(ZIO.attempt(EmbeddedKafkaService(EmbeddedKafka.start())))(_.stop())
-  }
-}
 
 object Main extends ZIOAppDefault {
 
