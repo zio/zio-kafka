86fa3486886485f02ead12b70b7ef0c02167f5c4
Erik van Oosten <e.vanoosten@grons.nl>
2025-04-20 10:31:31 +0200

    Revert "Test run: enable producer diagnostics in benchmark"
    This reverts commit f7b08c6c3503d226c4919cbcb6d2c30a7a96debe.

--- zio-kafka-bench/src/main/scala/zio/kafka/bench/ZioKafkaProducerBenchmark.scala
+++ zio-kafka-bench/src/main/scala/zio/kafka/bench/ZioKafkaProducerBenchmark.scala
@@ -3,7 +3,7 @@ package zio.kafka.bench
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.openjdk.jmh.annotations._
 import zio.kafka.admin.AdminClient.NewTopic
-import zio.kafka.producer.{ Producer, ProducerEvent }
+import zio.kafka.producer.Producer
 import zio.kafka.serde.Serde
 import zio.kafka.testkit.Kafka
 import zio.kafka.testkit.KafkaTestUtils
@@ -19,18 +19,8 @@ class ZioKafkaProducerBenchmark extends ProducerZioBenchmark[Kafka with Producer
     new ProducerRecord(topic1, k, v)
   })
 
-  private val producerDiagnostics: Producer.ProducerDiagnostics = new Producer.ProducerDiagnostics {
-    override def emit(event: => ProducerEvent): UIO[Unit] = ZIO.unit
-  }
-
   override protected def bootstrap: ZLayer[Any, Nothing, Kafka with Producer] =
-    ZLayer
-      .make[Kafka with Producer](
-        Kafka.embedded,
-        ZLayer.fromZIO(KafkaTestUtils.producerSettings).update(_.withDiagnostics(producerDiagnostics)),
-        Producer.live
-      )
-      .orDie
+    ZLayer.make[Kafka with Producer](Kafka.embedded, KafkaTestUtils.producer).orDie
 
   override def initialize: ZIO[Kafka & Producer, Throwable, Any] =
     ZIO.scoped {
