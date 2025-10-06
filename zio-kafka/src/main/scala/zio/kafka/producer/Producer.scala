e271d29e8f5138e94971ee774c5e7f80d279aa33
Erik van Oosten <e.vanoosten@grons.nl>
2025-10-06 13:23:18 +0200

    fmt

--- zio-kafka/src/main/scala/zio/kafka/producer/Producer.scala
+++ zio-kafka/src/main/scala/zio/kafka/producer/Producer.scala
@@ -267,7 +267,11 @@ object Producer {
       metrics  = new ZioProducerMetrics(settings.metricLabels)
       producer = new ProducerLive(settings, wrappedDiagnostics, javaProducer, runtime, sendQueue, metrics)
       // To prevent shutdown problems, the following fibers need to be interruptable (see #1588):
-      _ <- sendQueue.size.flatMap(metrics.observeSendQueueSize).schedule(Schedule.fixed(10.seconds)).interruptible.forkScoped
+      _ <- sendQueue.size
+             .flatMap(metrics.observeSendQueueSize)
+             .schedule(Schedule.fixed(10.seconds))
+             .interruptible
+             .forkScoped
       _ <- ZIO.blocking(producer.sendFromQueue).interruptible.forkScoped
     } yield producer
 
