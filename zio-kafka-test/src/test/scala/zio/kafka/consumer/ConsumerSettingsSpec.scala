5f8697e925c585e3a97ca8c600fc2e2a12248def
Erik van Oosten <e.vanoosten@grons.nl>
2025-10-12 20:13:58 +0200

    Validate consumer settings (#1580)
    Add new method `ConsumerSettings.validate` which checks a few illegal
    settings' values. The settings are checked when the consumer is created.
    
    Fixes #1579

--- zio-kafka-test/src/test/scala/zio/kafka/consumer/ConsumerSettingsSpec.scala
+++ zio-kafka-test/src/test/scala/zio/kafka/consumer/ConsumerSettingsSpec.scala
@@ -10,14 +10,72 @@ object ConsumerSettingsSpec extends ZIOSpecDefaultSlf4j {
   override def spec: Spec[TestEnvironment with Scope, Throwable] =
     suite("ConsumerSettingsSpec")(
       test("accepts no auto.commit") {
-        ZIO.attempt(ConsumerSettings(List("host"))) *> assertCompletesZIO
+        ConsumerSettings(List("host")).validate() *> assertCompletesZIO
       },
       test("accepts disabled auto.commit") {
-        ZIO.attempt(ConsumerSettings(List("host")).withProperty("enable.auto.commit", "false")) *> assertCompletesZIO
+        ConsumerSettings(List("host")).withProperty("enable.auto.commit", "false").validate() *> assertCompletesZIO
       },
       test("rejects auto.commit") {
-        val settings = ZIO.attempt(ConsumerSettings(List("host")).withProperty("enable.auto.commit", "true")).exit
-        assertZIO(settings)(failsWithA[IllegalArgumentException])
+        val settings = ConsumerSettings(List("host")).withProperty("enable.auto.commit", "true").validate().exit
+        assertZIO(settings)(
+          failsWithA[IllegalArgumentException] &&
+            fails(
+              hasMessage(
+                equalTo(
+                  "Invalid settings: Because zio-kafka does pre-fetching, auto commit is not supported. Please do not set config enable.auto.commit."
+                )
+              )
+            )
+        )
+      },
+      test("accepts commitTimeout > 2 * pollTimeout") {
+        Seq(2.minutes + 1.milli, 10.minutes).map { commitTimeout =>
+          ConsumerSettings(List("host"))
+            .withCommitTimeout(commitTimeout)
+            .withPollTimeout(1.minute)
+            .validate() *> assertCompletesZIO
+        }
+          .reduce(_ && _)
+      },
+      test("rejects commitTimeout <= 2 * pollTimeout") {
+        Seq(15.seconds, 2.minutes).map { commitTimeout =>
+          val settings = ConsumerSettings(List("host"))
+            .withCommitTimeout(commitTimeout)
+            .withPollTimeout(1.minute)
+            .validate()
+            .exit
+          assertZIO(settings)(
+            failsWithA[IllegalArgumentException] &&
+              fails(
+                hasMessage(
+                  equalTo(
+                    s"Invalid settings: Commit timeout must be larger than 2 * pollTimeout, saw commitTimeout ${commitTimeout} and pollTimeout PT1M."
+                  )
+                )
+              )
+          )
+        }
+          .reduce(_ && _)
+      },
+      test("rejects with multiple problems") {
+        val settings = ConsumerSettings(List("host"))
+          .withProperty("enable.auto.commit", "true")
+          .withCommitTimeout(15.seconds)
+          .withPollTimeout(1.minute)
+          .validate()
+          .exit
+        assertZIO(settings)(
+          failsWithA[IllegalArgumentException] &&
+            fails(
+              hasMessage(
+                equalTo(
+                  "Invalid settings: " +
+                    "Because zio-kafka does pre-fetching, auto commit is not supported. Please do not set config enable.auto.commit. " +
+                    "Commit timeout must be larger than 2 * pollTimeout, saw commitTimeout PT15S and pollTimeout PT1M."
+                )
+              )
+            )
+        )
       }
     )
 
